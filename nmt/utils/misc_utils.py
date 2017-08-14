b3f1bcc97caf388fc3cc3856fda8404e5c9e6f86
Rui Zhao <rzhao@google.com>
2017-08-14 02:22:43 -0400

    Fix print_out breaks when use Python2 and system encoding is not utf-8.
    PiperOrigin-RevId: 165144718

--- nmt/utils/misc_utils.py
+++ nmt/utils/misc_utils.py
@@ -60,7 +60,11 @@ def print_out(s, f=None, new_line=True):
       f.write(b"\n")
 
   # stdout
-  print(s, end="", file=sys.stdout)
+  out_s = s.encode("utf-8")
+  if not isinstance(out_s, str):
+    out_s = out_s.decode("utf-8")
+  print(out_s, end="", file=sys.stdout)
+
   if new_line:
     sys.stdout.write("\n")
   sys.stdout.flush()
