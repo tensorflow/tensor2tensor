6cb0bc8bea37cdbc31cc6260d9727c577bd3a278
T2T Team <no-reply@google.com>
2017-09-15 12:00:26 -0700

    Add ability to average the last N checkpoints, without needing to specify individual checkpoints.
    PiperOrigin-RevId: 168867603

--- tensor2tensor/utils/avg_checkpoints.py
+++ tensor2tensor/utils/avg_checkpoints.py
@@ -18,6 +18,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import os
+
 # Dependency imports
 
 import numpy as np
@@ -30,6 +32,9 @@ FLAGS = flags.FLAGS
 
 flags.DEFINE_string("checkpoints", "",
                     "Comma-separated list of checkpoints to average.")
+flags.DEFINE_integer("num_last_checkpoints", 0,
+                     "Averages the last N saved checkpoints."
+                     " If the checkpoints flag is set, this is ignored.")
 flags.DEFINE_string("prefix", "",
                     "Prefix (e.g., directory) to append to each checkpoint.")
 flags.DEFINE_string("output_path", "/tmp/averaged.ckpt",
@@ -42,17 +47,32 @@ def checkpoint_exists(path):
 
 
 def main(_):
-  # Get the checkpoints list from flags and run some basic checks.
-  checkpoints = [c.strip() for c in FLAGS.checkpoints.split(",")]
-  checkpoints = [c for c in checkpoints if c]
-  if not checkpoints:
-    raise ValueError("No checkpoints provided for averaging.")
-  if FLAGS.prefix:
-    checkpoints = [FLAGS.prefix + c for c in checkpoints]
+  if FLAGS.checkpoints:
+    # Get the checkpoints list from flags and run some basic checks.
+    checkpoints = [c.strip() for c in FLAGS.checkpoints.split(",")]
+    checkpoints = [c for c in checkpoints if c]
+    if not checkpoints:
+      raise ValueError("No checkpoints provided for averaging.")
+    if FLAGS.prefix:
+      checkpoints = [FLAGS.prefix + c for c in checkpoints]
+  else:
+    assert FLAGS.num_last_checkpoints >= 1, "Must average at least one model"
+    assert FLAGS.prefix, ("Prefix must be provided when averaging last"
+                          " N checkpoints")
+    checkpoint_state = tf.train.get_checkpoint_state(
+        os.path.dirname(FLAGS.prefix))
+    # Checkpoints are ordered from oldest to newest.
+    checkpoints = checkpoint_state.all_model_checkpoint_paths[
+        -FLAGS.num_last_checkpoints:]
+
   checkpoints = [c for c in checkpoints if checkpoint_exists(c)]
   if not checkpoints:
-    raise ValueError(
-        "None of the provided checkpoints exist. %s" % FLAGS.checkpoints)
+    if FLAGS.checkpoints:
+      raise ValueError(
+          "None of the provided checkpoints exist. %s" % FLAGS.checkpoints)
+    else:
+      raise ValueError("Could not find checkpoints at %s" %
+                       os.path.dirname(FLAGS.prefix))
 
   # Read variables from all checkpoints and average them.
   tf.logging.info("Reading variables and averaging checkpoints:")
