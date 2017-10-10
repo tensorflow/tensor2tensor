f2d6295c2c3550ee8061c704d37a305431225a9b
T2T Team <no-reply@google.com>
2017-10-10 11:03:18 -0700

    Add iterations_per_loop flag to tpu_trainer
    PiperOrigin-RevId: 171703463

--- tensor2tensor/tpu/tpu_trainer.py
+++ tensor2tensor/tpu/tpu_trainer.py
@@ -36,6 +36,8 @@ flags.DEFINE_integer("tpu_num_shards", 8, "Number of tpu shards.")
 flags.DEFINE_string("output_dir", "", "Base output directory for run.")
 flags.DEFINE_string("master", "", "Address of TensorFlow master.")
 flags.DEFINE_integer("eval_steps", 10, "Number of steps in evaluation.")
+flags.DEFINE_integer("iterations_per_loop", 1000,
+                     "Number of iterations in a TPU training loop.")
 
 
 def main(unused_argv):
@@ -59,7 +61,8 @@ def main(unused_argv):
       master=FLAGS.master,
       num_shards=FLAGS.tpu_num_shards,
       batch_size=hparams.tpu_batch_size_per_shard * FLAGS.tpu_num_shards,
-      log_device_placement=FLAGS.log_device_placement)
+      log_device_placement=FLAGS.log_device_placement,
+      iterations_per_loop=FLAGS.iterations_per_loop)
   estimator.train(
       lambda params: input_fn(tf.estimator.ModeKeys.TRAIN, params),
       steps=FLAGS.train_steps)
