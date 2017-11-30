5bf2af4015378f9fca3e2b1aaf0871900c1971fe
Noam Shazeer <noam@google.com>
2017-11-30 11:23:07 -0800

    Packed datasets - combine examples to constant length for efficient TPU training.  Modify transformer to keep the packed-together examples from attending to one another.
    PiperOrigin-RevId: 177481956

--- tensor2tensor/layers/common_attention.py
+++ tensor2tensor/layers/common_attention.py
@@ -614,6 +614,24 @@ def attention_bias_lower_triangle(length):
 
 
 @expert_utils.add_name_scope()
+def attention_bias_same_segment(query_segment_id, memory_segment_id):
+  """Create an bias tensor to be added to attention logits.
+
+  Positions with the same segment_ids can see each other.
+
+  Args:
+    query_segment_id: a float `Tensor` with shape [batch, query_length].
+    memory_segment_id: a float `Tensor` with shape [batch, memory_length].
+
+  Returns:
+    a `Tensor` with shape [batch, 1, query_length, memory_length].
+  """
+  ret = tf.to_float(tf.not_equal(tf.expand_dims(query_segment_id, 2),
+                                 tf.expand_dims(memory_segment_id, 1))) * -1e9
+  return tf.expand_dims(ret, axis=1)
+
+
+@expert_utils.add_name_scope()
 def attention_bias_ignore_padding(memory_padding):
   """Create an bias tensor to be added to attention logits.
 
