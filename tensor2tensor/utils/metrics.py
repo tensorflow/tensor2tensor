eb015dd2a2a321073c0388326c681a33d05289b1
Eric Purdy <eric.purdy@fathomhealth.co>
2017-12-01 22:43:04 +0000

    make metrics work with dicts

--- tensor2tensor/utils/metrics.py
+++ tensor2tensor/utils/metrics.py
@@ -430,6 +430,13 @@ def create_evaluation_metrics(problems, model_hparams):
 
     def problem_metric_fn(predictions, features, labels):
       """Metric fn."""
+      labels = features.get("targets", None)
+      problem_choice = features.get("problem_choice", 0)
+
+      # (epurdy/fathom)
+      if isinstance(predictions, dict):
+        predictions = predictions['logits']
+      
       # Send along the entire features dict if the metric fn has the kwarg
       # "features".
       kwargs = {}
