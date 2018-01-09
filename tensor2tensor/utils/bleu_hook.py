85312ec895ddbf1d46a5ab5e4e3cd8b7f1249209
Martin Popel <popel@ufal.mff.cuni.cz>
2018-01-09 01:13:04 +0100

    Scripts for proper BLEU evaluation, batch translation and averaging (#488)
    `t2t-bleu` computes the "real" BLEU
    (giving the same result as [sacréBLEU](https://github.com/awslabs/sockeye/tree/master/contrib/sacrebleu) with `--tokenization intl`
    and as [mteval-v14.pl](https://github.com/moses-smt/mosesdecoder/blob/master/scripts/generic/mteval-v14.pl) with `--international-tokenization`).
    It can be used in two ways:
    * To evaluate an already translated file: `t2t-bleu --translation=my-wmt13.de --reference=wmt13_deen.de`
    * To evaluate all translations in a given directory.
    
    `t2t-translate-all` translates all checkpoints in a given directory.
    A custom command (e.g. SGE cluster wrapper) can be used instead of `t2t-decoder` for the translation.
    
    `t2t-avg-all` for each checkpoint in a given directory
    it averages it with the N preceding ones.
    
    All three scripts wait a given number of minutes for new checkpoints
    (produced by t2t-decoder, which can be run concurrently with these scripts).

--- tensor2tensor/utils/bleu_hook.py
+++ tensor2tensor/utils/bleu_hook.py
@@ -20,9 +20,12 @@ from __future__ import print_function
 
 import collections
 import math
+import os
 import re
 import sys
+import time
 import unicodedata
+from collections import namedtuple
 
 # Dependency imports
 
@@ -197,3 +200,68 @@ def bleu_wrapper(ref_filename, hyp_filename, case_sensitive=False):
   ref_tokens = [bleu_tokenize(x) for x in ref_lines]
   hyp_tokens = [bleu_tokenize(x) for x in hyp_lines]
   return compute_bleu(ref_tokens, hyp_tokens)
+
+
+StepFile = namedtuple('StepFile', 'filename mtime ctime steps')
+
+
+def _read_stepfiles_list(path_prefix, path_suffix='.index', min_steps=0):
+  stepfiles = []
+  for filename in tf.gfile.Glob(path_prefix + '*-[0-9]*' + path_suffix):
+    basename = filename[:-len(path_suffix)] if len(path_suffix) else filename
+    try:
+      steps = int(basename.rsplit('-')[-1])
+    except ValueError:  # The -[0-9]* part is not an integer.
+      continue
+    if steps < min_steps:
+      continue
+    if not os.path.exists(filename):
+      tf.logging.info(filename + " was deleted, so skipping it")
+      continue
+    stepfiles.append(StepFile(basename, os.path.getmtime(filename),
+                              os.path.getctime(filename), steps))
+  return sorted(stepfiles, key=lambda x: -x.steps)
+
+
+def stepfiles_iterator(path_prefix, wait_minutes=0, min_steps=0,
+                       path_suffix='.index', sleep_sec=10):
+  """Continuously yield new files with steps in filename as they appear.
+
+  This is useful for checkpoint files or other files whose names differ just in an interger
+  marking the number of steps and match the wildcard path_prefix + '*-[0-9]*' + path_suffix.
+  Unlike `tf.contrib.training.checkpoints_iterator`, this
+  implementation always starts from the oldest files
+  (and it cannot miss any file). Note that the oldest checkpoint
+  may be deleted anytime by Tensorflow (if set up so). It is up to the user
+  to check that the files returned by this generator actually exist.
+  Args:
+    path_prefix: The directory + possible common filename prefix to the files.
+    path_suffix: Common filename suffix (after steps), including possible extension dot.
+    wait_minutes: The maximum amount of minutes to wait between files.
+    min_steps: Skip files with lower global step.
+    sleep_sec: How often to check for new files.
+  Yields:
+    named tuples (filename, mtime, ctime, steps) of the files as they arrive.
+  """
+  # Wildcard D*-[0-9]* does not match D/x-1, so if D is a directory let path_prefix='D/'.
+  if not path_prefix.endswith(os.sep) and os.path.isdir(path_prefix):
+    path_prefix += os.sep
+  stepfiles = _read_stepfiles_list(path_prefix, path_suffix, min_steps)
+  tf.logging.info("Found %d files with steps: %s"
+                  % (len(stepfiles), ", ".join(str(x.steps) for x in reversed(stepfiles))))
+  exit_time = time.time() + wait_minutes * 60
+  while True:
+    if not stepfiles and wait_minutes:
+      tf.logging.info('Waiting till %s if a new file matching %s*-[0-9]*%s appears'
+                      % (time.asctime(time.localtime(exit_time)), path_prefix, path_suffix))
+      while True:
+        stepfiles = _read_stepfiles_list(path_prefix, path_suffix, min_steps)
+        if stepfiles or time.time() > exit_time:
+          break
+        time.sleep(sleep_sec)
+    if not stepfiles:
+      return
+
+    stepfile = stepfiles.pop()
+    exit_time, min_steps = stepfile.ctime + wait_minutes * 60, stepfile.steps + 1
+    yield stepfile
