21dfc3e1f5d88c3ce4e9cd065e70ffc923a2ea95
Ryan Sepassi <rsepassi@google.com>
2018-01-12 14:48:51 -0800

    Internal merges
    PiperOrigin-RevId: 181796253

--- tensor2tensor/serving/query.py
+++ tensor2tensor/serving/query.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Query an exported model."""
+"""Query an exported model. Py2 only. Install tensorflow-serving-api."""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
