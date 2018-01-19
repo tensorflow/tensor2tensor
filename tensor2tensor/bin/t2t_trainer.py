02625ffb752907a2a0d795e987cb469fff58f583
Andrei Dinin <andreidinin@gmail.com>
2018-01-19 14:58:29 -0500

    updating the path using fhfile

--- tensor2tensor/bin/t2t_trainer.py
+++ tensor2tensor/bin/t2t_trainer.py
@@ -32,6 +32,8 @@ from tensor2tensor.utils import registry
 from tensor2tensor.utils import trainer_lib
 from tensor2tensor.utils import usr_dir
 
+from gcloud import fhfile
+
 import tensorflow as tf
 
 flags = tf.flags
@@ -67,7 +69,7 @@ try:
   # continuous_train_and_eval does not work with ValidationMonitor.
   flags.DEFINE_string("schedule", "train_and_evaluate",
                       "Method of Experiment to run.")
-  
+
   flags.DEFINE_integer("eval_steps", 10000,
                        "Number of steps in evaluation. By default, eval will "
                        "stop after eval_steps or when it runs through the eval "
@@ -90,7 +92,7 @@ flags.DEFINE_bool("debug_mode", False, "Truncate training for debug purposes")
 # NOTE: this is set as REQUIRED, in main()
 flags.DEFINE_string("airflow_pipeline_yaml", None,
     "For saving to assets.extra")
-flags.DEFINE_string("description", "", 
+flags.DEFINE_string("description", "",
     "Description for this run.  Used in model name.  E.g., 'special_softmax'.")
 ##################
 #
@@ -117,7 +119,7 @@ def create_experiment_fn():
   return trainer_lib.create_experiment_fn(
       model_name=FLAGS.model,
       problem_name=get_problem_name(),
-      data_dir=os.path.expanduser(FLAGS.data_dir),
+      data_dir=update_data_dir(os.path.expanduser(FLAGS.data_dir)),
       train_steps=FLAGS.train_steps,
       eval_steps=FLAGS.eval_steps,
       min_eval_frequency=FLAGS.local_eval_frequency,
@@ -132,6 +134,11 @@ def create_experiment_fn():
       eval_early_stopping_metric_minimize=FLAGS.
       eval_early_stopping_metric_minimize,
       use_tpu=FLAGS.use_tpu)
+
+
+def update_data_dir(data_dir):
+  fhfile = FHFile(data_dir)
+  return fhfile.name
 
 
 def create_run_config(hp):
@@ -167,7 +174,7 @@ def create_run_config(hp):
 
 def generate_data():
   # Generate data if requested.
-  data_dir = os.path.expanduser(FLAGS.data_dir)
+  data_dir = update_data_dir(os.path.expanduser(FLAGS.data_dir))
   tmp_dir = os.path.expanduser(FLAGS.tmp_dir)
   tf.gfile.MakeDirs(data_dir)
   tf.gfile.MakeDirs(tmp_dir)
@@ -331,7 +338,7 @@ def main(_):
   execute_schedule(exp)
 
   # Fathom
-  if not FLAGS.debug_mode and FLAGS.eval_early_stopping_steps is not None: 
+  if not FLAGS.debug_mode and FLAGS.eval_early_stopping_steps is not None:
     _pick_optimal_model()
   dir_path, model_name = upload_model_to_gcs(FLAGS=FLAGS)
 
