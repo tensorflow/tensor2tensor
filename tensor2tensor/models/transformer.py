4eaaf23d77476c8c33676d14b0ccc25a6385fdfe
Ryan Sepassi <rsepassi@google.com>
2018-02-08 11:16:13 -0800

    Support hyperparameter tuning on Cloud ML Engine (reapply)
    PiperOrigin-RevId: 185022560

--- tensor2tensor/models/transformer.py
+++ tensor2tensor/models/transformer.py
@@ -1228,11 +1228,9 @@ def transformer_prepend():
   return transformer_prepend_v2()
 
 
-@registry.register_ranged_hparams("transformer_base")
+@registry.register_ranged_hparams
 def transformer_base_range(rhp):
   """Small range of hyperparameters."""
-  hparams = transformer_base()
-  common_hparams.fill_ranged_hparams_from_hparams(hparams, rhp)
   # After starting from base, set intervals for some parameters.
   rhp.set_float("learning_rate", 0.3, 3.0, scale=rhp.LOG_SCALE)
   rhp.set_discrete("learning_rate_warmup_steps",
@@ -1337,8 +1335,6 @@ def transformer_tiny_tpu():
 @registry.register_ranged_hparams
 def transformer_tiny_tpu_range(rhp):
   """Small range of hyperparameters."""
-  hparams = transformer_tiny_tpu()
-  common_hparams.fill_ranged_hparams_from_hparams(hparams, rhp)
   rhp.set_float("learning_rate", 0.3, 3.0, scale=rhp.LOG_SCALE)
   rhp.set_float("weight_decay", 0.0, 2.0)
 
@@ -1346,8 +1342,6 @@ def transformer_tiny_tpu_range(rhp):
 @registry.register_ranged_hparams
 def transformer_tpu_range(rhp):
   """Small range of hyperparameters."""
-  hparams = transformer_tpu()
-  common_hparams.fill_ranged_hparams_from_hparams(hparams, rhp)
   # After starting from base, set intervals for some parameters.
   rhp.set_float("learning_rate", 0.3, 3.0, scale=rhp.LOG_SCALE)
   rhp.set_discrete("learning_rate_warmup_steps",
@@ -1356,13 +1350,6 @@ def transformer_tpu_range(rhp):
   rhp.set_float("optimizer_adam_beta1", 0.85, 0.95)
   rhp.set_float("optimizer_adam_beta2", 0.97, 0.99)
   rhp.set_float("weight_decay", 0.0, 2.0)
-
-
-@registry.register_ranged_hparams
-def transformer_tpu_batch_range(rhp):
-  hparams = transformer_tpu()
-  common_hparams.fill_ranged_hparams_from_hparams(hparams, rhp)
-  rhp.set_discrete("batch_size", [256, 512, 768, 1024])
 
 
 @registry.register_hparams
