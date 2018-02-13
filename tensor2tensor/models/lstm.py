cfb69f751aab6718f8ef00f2020bb219e568b86d
T2T Team <no-reply@google.com>
2018-02-13 07:07:07 -0800

    Add LSTMSeq2seqBidirectionalEncoder and LSTMSeq2seqAttentionBidirectionalEncoder models which are LSTMSeq2seq and LSTMSeq2seqAttention with a bidirectional lstm as the encoder.
    PiperOrigin-RevId: 185525630

--- tensor2tensor/models/lstm.py
+++ tensor2tensor/models/lstm.py
@@ -19,6 +19,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import copy
+
 # Dependency imports
 
 from tensor2tensor.layers import common_hparams
@@ -131,6 +133,99 @@ def lstm_seq2seq_internal_attention(inputs, targets, hparams, train):
     return tf.expand_dims(decoder_outputs, axis=2)
 
 
+def lstm_bid_encoder(inputs, hparams, train, name):
+  """Bidirectional LSTM for encoding inputs that are [batch x time x size]."""
+
+  def dropout_lstm_cell():
+    return tf.contrib.rnn.DropoutWrapper(
+        tf.contrib.rnn.BasicLSTMCell(hparams.hidden_size),
+        input_keep_prob=1.0 - hparams.dropout * tf.to_float(train))
+
+  with tf.variable_scope(name):
+    cell_fw = tf.contrib.rnn.MultiRNNCell(
+        [dropout_lstm_cell() for _ in range(hparams.num_hidden_layers)])
+
+    cell_bw = tf.contrib.rnn.MultiRNNCell(
+        [dropout_lstm_cell() for _ in range(hparams.num_hidden_layers)])
+
+    ((encoder_fw_outputs, encoder_bw_outputs),
+     (encoder_fw_state, encoder_bw_state)) = tf.nn.bidirectional_dynamic_rnn(
+         cell_fw=cell_fw,
+         cell_bw=cell_bw,
+         inputs=inputs,
+         dtype=tf.float32,
+         time_major=False)
+
+    encoder_outputs = tf.concat((encoder_fw_outputs, encoder_bw_outputs), 2)
+    encoder_states = []
+
+    for i in range(hparams.num_hidden_layers):
+      if isinstance(encoder_fw_state[i], tf.contrib.rnn.LSTMStateTuple):
+        encoder_state_c = tf.concat(
+            values=(encoder_fw_state[i].c, encoder_bw_state[i].c),
+            axis=1,
+            name="encoder_fw_state_c")
+        encoder_state_h = tf.concat(
+            values=(encoder_fw_state[i].h, encoder_bw_state[i].h),
+            axis=1,
+            name="encoder_fw_state_h")
+        encoder_state = tf.contrib.rnn.LSTMStateTuple(
+            c=encoder_state_c, h=encoder_state_h)
+      elif isinstance(encoder_fw_state[i], tf.Tensor):
+        encoder_state = tf.concat(
+            values=(encoder_fw_state[i], encoder_bw_state[i]),
+            axis=1,
+            name="bidirectional_concat")
+
+      encoder_states.append(encoder_state)
+
+    encoder_states = tuple(encoder_states)
+    return encoder_outputs, encoder_states
+
+
+def lstm_seq2seq_internal_bid_encoder(inputs, targets, hparams, train):
+  """The basic LSTM seq2seq model with bidirectional encoder."""
+  with tf.variable_scope("lstm_seq2seq_bid_encoder"):
+    if inputs is not None:
+      # Flatten inputs.
+      inputs = common_layers.flatten4d3d(inputs)
+      # LSTM encoder.
+      _, final_encoder_state = lstm_bid_encoder(
+          tf.reverse(inputs, axis=[1]), hparams, train, "encoder")
+    else:
+      final_encoder_state = None
+    # LSTM decoder.
+    shifted_targets = common_layers.shift_right(targets)
+    hparams_decoder = copy.copy(hparams)
+    hparams_decoder.hidden_size = 2 * hparams.hidden_size
+    decoder_outputs, _ = lstm(
+        common_layers.flatten4d3d(shifted_targets),
+        hparams_decoder,
+        train,
+        "decoder",
+        initial_state=final_encoder_state)
+    return tf.expand_dims(decoder_outputs, axis=2)
+
+
+def lstm_seq2seq_internal_attention_bid_encoder(inputs, targets, hparams,
+                                                train):
+  """LSTM seq2seq model with attention, main step used for training."""
+  with tf.variable_scope("lstm_seq2seq_attention_bid_encoder"):
+    # Flatten inputs.
+    inputs = common_layers.flatten4d3d(inputs)
+    # LSTM encoder.
+    encoder_outputs, final_encoder_state = lstm_bid_encoder(
+        tf.reverse(inputs, axis=[1]), hparams, train, "encoder")
+    # LSTM decoder with attention
+    shifted_targets = common_layers.shift_right(targets)
+    hparams_decoder = copy.copy(hparams)
+    hparams_decoder.hidden_size = 2 * hparams.hidden_size
+    decoder_outputs, _ = lstm_attention_decoder(
+        common_layers.flatten4d3d(shifted_targets), hparams_decoder, train,
+        "decoder", final_encoder_state, encoder_outputs)
+    return tf.expand_dims(decoder_outputs, axis=2)
+
+
 @registry.register_model
 class LSTMSeq2seq(t2t_model.T2TModel):
 
@@ -152,6 +247,30 @@ class LSTMSeq2seqAttention(t2t_model.T2TModel):
       raise ValueError("LSTM models fail with orthogonal initializer.")
     train = self._hparams.mode == tf.estimator.ModeKeys.TRAIN
     return lstm_seq2seq_internal_attention(
+        features.get("inputs"), features["targets"], self._hparams, train)
+
+
+@registry.register_model
+class LSTMSeq2seqBidirectionalEncoder(t2t_model.T2TModel):
+
+  def body(self, features):
+    # TODO(lukaszkaiser): investigate this issue and repair.
+    if self._hparams.initializer == "orthogonal":
+      raise ValueError("LSTM models fail with orthogonal initializer.")
+    train = self._hparams.mode == tf.estimator.ModeKeys.TRAIN
+    return lstm_seq2seq_internal_bid_encoder(
+        features.get("inputs"), features["targets"], self._hparams, train)
+
+
+@registry.register_model
+class LSTMSeq2seqAttentionBidirectionalEncoder(t2t_model.T2TModel):
+
+  def body(self, features):
+    # TODO(lukaszkaiser): investigate this issue and repair.
+    if self._hparams.initializer == "orthogonal":
+      raise ValueError("LSTM models fail with orthogonal initializer.")
+    train = self._hparams.mode == tf.estimator.ModeKeys.TRAIN
+    return lstm_seq2seq_internal_attention_bid_encoder(
         features.get("inputs"), features["targets"], self._hparams, train)
 
 
--- tensor2tensor/models/lstm_test.py
+++ tensor2tensor/models/lstm_test.py
@@ -71,6 +71,46 @@ class LSTMTest(tf.test.TestCase):
       res = session.run(logits)
     self.assertEqual(res.shape, (3, 6, 1, 1, vocab_size))
 
+  def testLSTMSeq2seqBidirectionalEncoder(self):
+    vocab_size = 9
+    x = np.random.random_integers(1, high=vocab_size - 1, size=(3, 5, 1, 1))
+    y = np.random.random_integers(1, high=vocab_size - 1, size=(3, 6, 1, 1))
+    hparams = lstm.lstm_seq2seq()
+    p_hparams = problem_hparams.test_problem_hparams(vocab_size, vocab_size)
+    with self.test_session() as session:
+      features = {
+          "inputs": tf.constant(x, dtype=tf.int32),
+          "targets": tf.constant(y, dtype=tf.int32),
+      }
+      model = lstm.LSTMSeq2seqBidirectionalEncoder(
+          hparams, tf.estimator.ModeKeys.TRAIN, p_hparams)
+      logits, _ = model(features)
+      session.run(tf.global_variables_initializer())
+      res = session.run(logits)
+    self.assertEqual(res.shape, (3, 6, 1, 1, vocab_size))
+
+  def testLSTMSeq2seqAttentionBidirectionalEncoder(self):
+    vocab_size = 9
+    x = np.random.random_integers(1, high=vocab_size - 1, size=(3, 5, 1, 1))
+    y = np.random.random_integers(1, high=vocab_size - 1, size=(3, 6, 1, 1))
+    hparams = lstm.lstm_attention()
+
+    p_hparams = problem_hparams.test_problem_hparams(vocab_size, vocab_size)
+    x = tf.constant(x, dtype=tf.int32)
+    x = tf.placeholder_with_default(x, shape=[None, None, 1, 1])
+
+    with self.test_session() as session:
+      features = {
+          "inputs": x,
+          "targets": tf.constant(y, dtype=tf.int32),
+      }
+      model = lstm.LSTMSeq2seqAttentionBidirectionalEncoder(
+          hparams, tf.estimator.ModeKeys.TRAIN, p_hparams)
+      logits, _ = model(features)
+      session.run(tf.global_variables_initializer())
+      res = session.run(logits)
+    self.assertEqual(res.shape, (3, 6, 1, 1, vocab_size))
+
 
 if __name__ == "__main__":
   tf.test.main()
