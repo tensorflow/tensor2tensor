b83b6db1b2251c9fd5a44b72eede8cee56f89a20
Brian Barnes <bgb@google.com>
2018-04-03 16:52:31 -0700

    support prediction requests to CMLE and adding serving_utils to decouple serving logic from query.py
    PiperOrigin-RevId: 191520955

--- tensor2tensor/serving/README.md
+++ tensor2tensor/serving/README.md
@@ -51,3 +51,55 @@ t2t-query-server \
   --problem=translate_ende_wmt8k \
   --data_dir=~/t2t/data
 ```
+
+
+## Serve Predictions with Cloud ML Engine
+
+Alternatively, you can deploy a model on Cloud ML Engine to serve predictions.
+To do so, export the model as in Step 1, then do the following:
+
+[Install gcloud](https://cloud.google.com/sdk/downloads)
+
+#### Copy exported model to Google Cloud Storage
+
+```
+ORIGIN=<your_gcs_path>
+EXPORTS_PATH=/tmp/t2t_train/export/Servo
+LATEST_EXPORT=${EXPORTS_PATH}/$(ls ${EXPORTS_PATH} | tail -1)
+gsutil cp -r ${LATEST_EXPORT}/* $ORIGIN
+```
+
+#### Create a model
+
+```
+MODEL_NAME=t2t_test
+gcloud ml-engine models create $MODEL_NAME
+```
+
+This step only needs to be performed once.
+
+#### Create a model version
+
+```
+VERSION=v0
+gcloud ml-engine versions create $VERSION \
+  --model $MODEL_NAME \
+  --runtime-version 1.6 \
+  --origin $ORIGIN
+```
+
+**NOTE:** Due to overhead from VM warmup, prediction requests may timeout. To
+mitigate this issue, provide a [YAML configuration
+file](https://cloud.google.com/sdk/gcloud/reference/ml-engine/versions/create)
+via the `--config flag`, with `minNodes > 0`. These nodes are always on, and
+will be billed accordingly.
+
+#### Query Cloud ML Engine
+
+```
+t2t-query-server \
+  --cloud_mlengine_model_name $MODEL_NAME \
+  --cloud_mlengine_model_version $VERSION \
+  --problem translate_ende_wmt8k \
+  --data_dir ~/t2t/data
+```
--- tensor2tensor/serving/query.py
+++ tensor2tensor/serving/query.py
@@ -20,25 +20,24 @@ from __future__ import print_function
 
 import os
 
-# Dependency imports
-
-from grpc.beta import implementations
-
+from oauth2client.client import GoogleCredentials
 from six.moves import input  # pylint: disable=redefined-builtin
 
 from tensor2tensor import problems as problems_lib  # pylint: disable=unused-import
-from tensor2tensor.data_generators import text_encoder
+from tensor2tensor.serving import serving_utils
 from tensor2tensor.utils import registry
 from tensor2tensor.utils import usr_dir
-
 import tensorflow as tf
-
-from tensorflow_serving.apis import predict_pb2
-from tensorflow_serving.apis import prediction_service_pb2
 
 flags = tf.flags
 FLAGS = flags.FLAGS
 
+flags.DEFINE_string("cloud_mlengine_model_name", None,
+                    "Name of model deployed on Cloud ML Engine.")
+flags.DEFINE_string(
+    "cloud_mlengine_model_version", None,
+    "Version of the model to use. If None, requests will be "
+    "sent to the default version.")
 flags.DEFINE_string("server", None, "Address to Tensorflow Serving server.")
 flags.DEFINE_string("servable_name", None, "Name of served model.")
 flags.DEFINE_string("problem", None, "Problem name.")
@@ -48,69 +47,37 @@ flags.DEFINE_string("inputs_once", None, "Query once with this input.")
 flags.DEFINE_integer("timeout_secs", 10, "Timeout for query.")
 
 
-def make_example(input_ids, feature_name="inputs"):
-  features = {
-      feature_name:
-          tf.train.Feature(int64_list=tf.train.Int64List(value=input_ids))
-  }
-  return tf.train.Example(features=tf.train.Features(feature=features))
-
-
-def create_stub():
-  host, port = FLAGS.server.split(":")
-  channel = implementations.insecure_channel(host, int(port))
-  return prediction_service_pb2.beta_create_PredictionService_stub(channel)
-
-
-# TODO(bgb): Refactor to support requests to CMLE and update docs accordingly.
-def query(stub, input_ids, feature_name="inputs"):
-  request = predict_pb2.PredictRequest()
-  request.model_spec.name = FLAGS.servable_name
-  ex = make_example(input_ids, feature_name)
-  request.inputs["input"].CopyFrom(
-      tf.contrib.util.make_tensor_proto(ex.SerializeToString(), shape=[1]))
-  response = stub.Predict(request, FLAGS.timeout_secs)
-  output_ids = response.outputs["outputs"].int_val
-  return output_ids
-
-
-def encode(inputs, encoder):
-  input_ids = encoder.encode(inputs)
-  input_ids.append(text_encoder.EOS_ID)
-  return input_ids
-
-
-def decode(output_ids, output_decoder):
-  return output_decoder.decode(output_ids)
+def validate_flags():
+  """Validates flags are set to acceptable values."""
+  if FLAGS.cloud_mlengine_model_name:
+    assert not FLAGS.server
+    assert not FLAGS.servable_name
+  else:
+    assert FLAGS.server
+    assert FLAGS.servable_name
 
 
 def main(_):
   tf.logging.set_verbosity(tf.logging.INFO)
+  validate_flags()
   usr_dir.import_usr_dir(FLAGS.t2t_usr_dir)
-
   problem = registry.problem(FLAGS.problem)
   hparams = tf.contrib.training.HParams(
       data_dir=os.path.expanduser(FLAGS.data_dir))
   problem.get_hparams(hparams)
-
-  fname = "inputs" if problem.has_inputs else "targets"
-  input_encoder = problem.feature_info[fname].encoder
-  output_decoder = problem.feature_info["targets"].encoder
-
-  stub = create_stub()
-
+  if FLAGS.cloud_mlengine_model_name:
+    request_fn = serving_utils.make_cloud_mlengine_request_fn(
+        credentials=GoogleCredentials.get_application_default(),
+        model_name=FLAGS.cloud_mlengine_model_name,
+        version=FLAGS.cloud_mlengine_model_version)
+  else:
+    request_fn = serving_utils.make_grpc_request_fn(
+        servable_name=FLAGS.servable_name,
+        server=FLAGS.server,
+        timeout_secs=FLAGS.timeout_secs)
   while True:
-    prompt = ">> "
-    if FLAGS.inputs_once:
-      inputs = FLAGS.inputs_once
-    else:
-      inputs = input(prompt)
-
-    input_ids = encode(inputs, input_encoder)
-    output_ids = query(stub, input_ids, feature_name=fname)
-
-    outputs = decode(output_ids, output_decoder)
-
+    inputs = FLAGS.inputs_once if FLAGS.inputs_once else input(">> ")
+    outputs = serving_utils.predict([inputs], problem, request_fn)
     print_str = """
 Input:
 {inputs}
@@ -118,12 +85,11 @@ Input:
 Output:
 {outputs}
     """
-    print(print_str.format(inputs=inputs, outputs=outputs))
+    print(print_str.format(inputs=inputs, outputs=outputs[0]))
     if FLAGS.inputs_once:
       break
 
 
 if __name__ == "__main__":
-  flags.mark_flags_as_required(
-      ["server", "servable_name", "problem", "data_dir"])
+  flags.mark_flags_as_required(["problem", "data_dir"])
   tf.app.run()
