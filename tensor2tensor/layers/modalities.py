88738a54bdb8fff65e5a20a3a1c8b537d538bb39
cbockman <cbockman@gmail.com>
2018-04-09 19:00:27 +0000

    more t2t updates

--- tensor2tensor/layers/modalities.py
+++ tensor2tensor/layers/modalities.py
@@ -28,6 +28,7 @@ from tensor2tensor.utils import registry
 
 import tensorflow as tf
 
+from tensorflow.python.eager import context
 
 @registry.register_symbol_modality("default")
 class SymbolModality(modality.Modality):
@@ -208,13 +209,13 @@ class ImageModality(modality.Modality):
   def bottom(self, inputs):
     with tf.variable_scope(self.name):
       inputs = tf.to_float(inputs)
-      if not tf.contrib.eager.in_eager_mode():
+      if not context.in_eager_mode():
         tf.summary.image("inputs", inputs, max_outputs=2)
       return inputs
 
   def targets_bottom(self, inputs):
     with tf.variable_scope(self.name):
-      if not tf.contrib.eager.in_eager_mode():
+      if not context.in_eager_mode():
         tf.summary.image("targets_bottom",
                          tf.cast(inputs, tf.uint8), max_outputs=1)
       inputs_shape = common_layers.shape_list(inputs)
@@ -237,7 +238,11 @@ class ImageModality(modality.Modality):
 
   def top(self, body_output, _):
     # TODO(lukaszkaiser): is this a universal enough way to get channels?
+<<<<<<< d1060bdaf3e1bda7468325a7d517fda13a728c55
     num_channels = self._model_hparams.problem.num_channels
+=======
+    num_channels = self._model_hparams.problem_instances[0].num_channels
+>>>>>>> more t2t updates
     with tf.variable_scope("rgb_softmax"):
       body_output_shape = common_layers.shape_list(body_output)
       reshape_shape = body_output_shape[:3]
@@ -255,7 +260,12 @@ class ImageModality(modality.Modality):
         logits,
         targets,
         self._model_hparams.label_smoothing,
+<<<<<<< d1060bdaf3e1bda7468325a7d517fda13a728c55
         weights_fn=self.targets_weights_fn)
+=======
+        weights_fn=self.targets_weights_fn,
+        gaussian=True)
+>>>>>>> more t2t updates
 
 
 @registry.register_image_modality("image_channel_compress")
@@ -334,7 +344,11 @@ class ImageChannelEmbeddingsBottom(modality.Modality):
     rgb_embedding_var = tf.identity(rgb_embedding_var)
     rgb_embedding_var *= float(hidden_size)**0.5
     channel_target_embs = []
+<<<<<<< d1060bdaf3e1bda7468325a7d517fda13a728c55
     for i in range(io_depth):
+=======
+    for i in xrange(io_depth):
+>>>>>>> more t2t updates
       # Adding the channel offsets to get the right embedding since the
       # embedding tensor has shape 256 * io_depth, hidden_size
       target_ids = tf.squeeze(targets_split[i], axis=3) + i * 256
@@ -668,6 +682,7 @@ class SigmoidClassLabelModality(ClassLabelModality):
   @property
   def name(self):
     return "sigmoid_class_symbol_modality_%d_%d" % (self._vocab_size,
+<<<<<<< d1060bdaf3e1bda7468325a7d517fda13a728c55
                                                     self._body_input_depth)
 
   def loss(self, top_out, targets):
@@ -711,6 +726,13 @@ class SigmoidMaxPoolingClassLabelModality(ClassLabelModality):
     # last dimension of num-classes represents binary labels for each class
     loss_scale = tf.losses.sigmoid_cross_entropy(
         multi_class_labels=targets, logits=top_out)
+=======
+                                                    self.body_input_depth)
+
+  def loss(self, top_out, targets):
+    loss_scale = tf.nn.sigmoid_cross_entropy_with_logits(
+        labels=targets, logits=top_out, name="SigmoidCrossEntropy")
+>>>>>>> more t2t updates
     # Weigh all classes equally
     weights = self.targets_weights_fn(targets)
     loss_denom = tf.reduce_sum(weights)
