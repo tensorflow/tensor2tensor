5bfe9306a256737d71301ba4f558b52348eee046
Eric Purdy <eric.purdy@fathomhealth.co>
2018-05-09 18:10:07 +0000

    many fixes to test script, switched to t2t_test image

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,10 +1,23 @@
-docker pull gcr.io/fathom-containers/t2tgpu
+# to run on dev machine:
+#USER=cluster
+# to run on circleci:
+USER=circleci
 
-docker run -it \
-       -v /home/circleci/diseaseTools:/usr/src/diseaseTools \
-       -v /home/circleci/tensor2tensor:/usr/src/tensor2tensor \
+T2T=tensor2tensor
+DT=diseaseTools
+IMAGE=gcr.io/fathom-containers/t2t_test
+GCS_KEY_NAME=GOOGLE_APPLICATION_CREDENTIALS
+GCS_KEY_PATH=/usr/src/diseaseTools/gcloud/keys/google-auth.json
+
+docker pull $IMAGE
+
+nvidia-docker run -it \
+       -v /home/$USER/$DT:/usr/src/diseaseTools \
+       -v /home/$USER/$T2T:/usr/src/tensor2tensor \
        -w /usr/src/tensor2tensor \
-       gcr.io/fathom-containers/t2tgpu \
+       --env PYTHONPATH=/usr/src/tensor2tensor:/usr/src/diseaseTools \
+       --env $GCS_KEY_NAME=$GCS_KEY_PATH \
+       $IMAGE \
        python3 -m pytest -vv \
        --ignore=tensor2tensor/utils/registry_test.py \
        --ignore=tensor2tensor/utils/trainer_lib_test.py \
@@ -15,4 +28,3 @@ docker run -it \
        --ignore=tensor2tensor/models/research/r_transformer_test.py \
        --junitxml=/usr/src/t2t/test_results/pytest/unittests.xml \
        /usr/src/tensor2tensor/tensor2tensor
-
