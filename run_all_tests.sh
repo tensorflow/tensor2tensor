81e8775b8a6ad0845a288afc52884f8414ccfaa7
Eric Purdy <eric.purdy@fathomhealth.co>
2018-05-09 17:15:14 +0000

    trying to fix test script

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -5,7 +5,7 @@ docker run -it \
        -v /home/circlci/tensor2tensor:/usr/src/tensor2tensor \
        -w /usr/src/tensor2tensor \
        gcr.io/fathom-containers/t2tgpu \
-       python3 -m pytest \
+       python3 -m pytest -vv \
        --ignore=tensor2tensor/utils/registry_test.py \
        --ignore=tensor2tensor/utils/trainer_lib_test.py \
        --ignore=tensor2tensor/visualization/visualization_test.py \
@@ -13,4 +13,6 @@ docker run -it \
        --ignore=tensor2tensor/bin/t2t_trainer_test.py \
        --ignore=tensor2tensor/data_generators/algorithmic_math_test.py \
        --ignore=tensor2tensor/models/research/r_transformer_test.py \
-       tensor2tensor
+       --junitxml=/usr/src/t2t/test_results/pytest/unittests.xml \
+       /usr/src/tensor2tensor/tensor2tensor
+
