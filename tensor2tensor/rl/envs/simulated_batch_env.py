23bd82f575fbe93d358350591269d8e18d2a24e1
Błażej O <blazej.osinski@codilime.com>
2018-05-17 02:54:41 +0200

    Making sure that random starts are indeed random.

--- tensor2tensor/rl/envs/simulated_batch_env.py
+++ tensor2tensor/rl/envs/simulated_batch_env.py
@@ -124,8 +124,9 @@ class SimulatedBatchEnv(in_graph_batch_env.InGraphBatchEnv):
     self.action_dtype = tf.int32
 
     if simulation_random_starts:
-      dataset = problem.dataset(tf.estimator.ModeKeys.TRAIN, FLAGS.data_dir,
-                                shuffle_files=True)
+      dataset = problem.dataset(
+        tf.estimator.ModeKeys.TRAIN, FLAGS.data_dir,
+        shuffle_files=True).skip(10).shuffle(buffer_size=100)
     else:
       dataset = problem.dataset(tf.estimator.ModeKeys.TRAIN, FLAGS.data_dir,
                                 shuffle_files=False).take(1)
