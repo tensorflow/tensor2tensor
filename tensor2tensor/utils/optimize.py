d7f17bb81bbedaa80502927fa0ac58cc08a0d7d3
Eric Purdy <eric.purdy@fathomhealth.co>
2018-05-24 16:45:29 +0000

    log gradient dtype

--- tensor2tensor/utils/optimize.py
+++ tensor2tensor/utils/optimize.py
@@ -113,6 +113,7 @@ class ConditionalOptimizer(tf.train.Optimizer):
       # Fathom: Ryan Sepassi said this would help
       if v.dtype == g.dtype:
         return (g, v)
+      print('compute_gradients', v.dtype, g.dtype)
       return (tf.cast(g, v.dtype), v)
     gradients = [cast_grad(g, v) for g, v in gradients]
     return gradients
