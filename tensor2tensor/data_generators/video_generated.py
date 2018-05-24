0c1d9850caba8acb8bda5dadc9c84ba9c6993a4e
T2T Team <no-reply@google.com>
2018-05-23 18:13:27 -0700

    Adding stochastic shape video data generator.
    PiperOrigin-RevId: 197823190

--- tensor2tensor/data_generators/video_generated.py
+++ tensor2tensor/data_generators/video_generated.py
@@ -18,6 +18,10 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import math
+import google3
+
+import matplotlib.pyplot as plt
 import numpy as np
 
 from tensor2tensor.data_generators import video_utils
@@ -58,6 +62,10 @@ class VideoStochasticShapes10k(video_utils.VideoProblem):
     return 10000
 
   @property
+  def video_length(self):
+    return 5
+
+  @property
   def extra_reading_spec(self):
     """Additional data fields to store on disk and their decoders."""
     data_fields = {
@@ -77,10 +85,102 @@ class VideoStochasticShapes10k(video_utils.VideoProblem):
     }
     p.target_modality = ("video", 256)
 
+  @staticmethod
+  def get_circle(x, y, z, c, s):
+    """Draws a circle with center(x, y), color c, size s and z-order of z."""
+    cir = plt.Circle((x, y), s, fc=c, zorder=z)
+    return cir
+
+  @staticmethod
+  def get_rectangle(x, y, z, c, s):
+    """Draws a rectangle with center(x, y), color c, size s and z-order of z."""
+    rec = plt.Rectangle((x-s, y-s), s*2.0, s*2.0, fc=c, zorder=z)
+    return rec
+
+  @staticmethod
+  def get_triangle(x, y, z, c, s):
+    """Draws a triangle with center (x, y), color c, size s and z-order of z."""
+    points = np.array([[0, 0], [s, s*math.sqrt(3.0)], [s*2.0, 0]])
+    tri = plt.Polygon(points + [x-s, y-s], fc=c, zorder=z)
+    return tri
+
+  def generate_stochastic_shape_instance(self):
+    """Yields one video of a shape moving to a random direction.
+
+       The size and color of the shapes are random but
+       consistent in a single video. The speed is fixed.
+
+    Raises:
+       ValueError: The frame size is not square.
+    """
+    if self.frame_height != self.frame_width or self.frame_height % 2 != 0:
+      raise ValueError("Generator only supports square frames with even size.")
+
+    lim = 10.0
+    direction = np.array([[+1.0, +1.0],
+                          [+1.0, +0.0],
+                          [+1.0, -1.0],
+                          [+0.0, +1.0],
+                          [+0.0, -1.0],
+                          [-1.0, +1.0],
+                          [-1.0, +0.0],
+                          [-1.0, -1.0]
+                         ])
+
+    rnd = np.random.randint(len(direction))
+    sp = np.array([lim/2.0, lim/2.0])
+    di = direction[rnd]
+
+    colors = ["b", "g", "r", "c", "m", "y", "k"]
+    color = np.random.choice(colors)
+
+    shape = np.random.choice([
+        VideoStochasticShapes10k.get_circle,
+        VideoStochasticShapes10k.get_rectangle,
+        VideoStochasticShapes10k.get_triangle])
+    speed = 1.0
+
+    size = np.random.uniform(0.5, 1.5)
+
+    back_color = str(0.0)
+    plt.ioff()
+
+    xy = np.array(sp)
+    di = direction[0]
+
+    for _ in range(self.video_length):
+      fig = plt.figure()
+      fig.set_dpi(self.frame_height//2)
+      fig.set_size_inches(2, 2)
+      ax = plt.axes(xlim=(0, lim), ylim=(0, lim))
+
+      # Background
+      ax.add_patch(VideoStochasticShapes10k.get_rectangle(
+          0.0, 0.0, -1.0, back_color, 25.0))
+      # Foreground
+      ax.add_patch(shape(xy[0], xy[1], 0.0, color, size))
+
+      plt.axis("off")
+      plt.tight_layout(pad=-2.0)
+      fig.canvas.draw()
+      image = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep="")
+      image = image.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+      image = np.copy(np.uint8(image))
+
+      plt.close()
+      xy += speed * di
+
+      yield image
+
   def generate_samples(self, data_dir, tmp_dir, unused_dataset_split):
     frame_number = 0
-    for _ in range(self.total_number_of_frames):
-      frame = np.zeros([self.frame_height, self.frame_width, self.num_channels],
-                       dtype=np.uint8)
-      yield {"frame": frame, "frame_number": [frame_number]}
-      frame_number += 1
+    done = False
+    while not done:
+      for frame_number, frame in enumerate(
+          self.generate_stochastic_shape_instance()):
+        if frame_number >= self.total_number_of_frames:
+          done = True
+          break
+
+        yield {"frame": frame, "frame_number": [frame_number]}
+        frame_number += 1
