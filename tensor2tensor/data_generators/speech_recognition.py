Fix CMVN variance normalization
Variance normalization should be performed dividing by the square root of the variance, not the variance

tensor2tensor/data_generators/speech_recognition.py
@@ -310,10 +310,10 @@ class SpeechRecognitionProblem(problem.Problem):
       assert fbank_size[0] == 1
 
       # This replaces CMVN estimation on data
-
+      var_epsilon = 1e-09
       mean = tf.reduce_mean(mel_fbanks, keepdims=True, axis=1)
-      variance = tf.reduce_mean((mel_fbanks-mean)**2, keepdims=True, axis=1)
-      mel_fbanks = (mel_fbanks - mean) / variance
+      variance = tf.reduce_mean(tf.square(mel_fbanks - mean), keepdims=True, axis=1)
+      mel_fbanks = (mel_fbanks - mean) * tf.rsqrt(variance + var_epsilon)
 
       # Later models like to flatten the two spatial dims. Instead, we add a
       # unit spatial dim and flatten the frequencies and channels.
@@ -377,13 +377,14 @@ class SpeechRecognitionModality(modality.Modality):
               nonpadding_mask) * num_mel_bins * num_channels
 
           # This replaces CMVN estimation on data
+          var_epsilon = 1e-09
           mean = tf.reduce_sum(
               x, axis=[1], keepdims=True) / num_of_nonpadding_elements
           variance = (num_of_nonpadding_elements * mean**2. -
                       2. * mean * tf.reduce_sum(x, axis=[1], keepdims=True) +
                       tf.reduce_sum(x**2, axis=[1], keepdims=True)
                      ) / num_of_nonpadding_elements
-          x = (x - mean) / variance * tf.expand_dims(nonpadding_mask, -1)
+          x = (x - mean) * tf.rsqrt(variance + var_epsilon) * tf.expand_dims(nonpadding_mask, -1)
       else:
         x = inputs
 