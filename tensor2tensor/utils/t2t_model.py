a0addd067cef1ceda5dd8936c4d8cf5a05156b25
Noam Shazeer <noam@google.com>
2018-07-20 14:43:00 -0700

    Turn off label smoothing for eval.
    PiperOrigin-RevId: 205456357

--- tensor2tensor/utils/t2t_model.py
+++ tensor2tensor/utils/t2t_model.py
@@ -472,7 +472,7 @@ class T2TModel(base.Layer):
     # When not in training mode, set all forms of dropout to zero.
     if mode != tf.estimator.ModeKeys.TRAIN:
       for key in hparams.values():
-        if key.endswith("dropout"):
+        if key.endswith("dropout") or key == "label_smoothing":
           log_info("Setting hparams.%s to 0.0", key)
           setattr(hparams, key, 0.0)
     self._hparams = hparams
