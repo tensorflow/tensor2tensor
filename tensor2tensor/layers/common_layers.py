7047c94abf1f9653774b5c478b8d9e6732c95225
T2T Team <no-reply@google.com>
2018-07-25 17:13:45 -0700

    zero add
    PiperOrigin-RevId: 206081798

--- tensor2tensor/layers/common_layers.py
+++ tensor2tensor/layers/common_layers.py
@@ -730,12 +730,27 @@ def apply_norm(x, norm_type, depth, epsilon):
                    "'noam', 'lr', 'none'.")
 
 
-def zero_add(previous_value, x, name=None, reuse=False):
-  """Resnet connection with zero initialization."""
-  with tf.name_scope(
-      name, default_name="zero_add", values=[previous_value, x], reuse=reuse):
+def zero_add(previous_value, x, name=None, reuse=None):
+  """Resnet connection with zero initialization.
+
+  Another type of resnet connection which returns previous_value + gamma * x.
+  gamma is a trainable scalar and initialized with zero. It is useful when a
+  module is plugged into a trained model and we want to make sure it matches the
+  original model's performance.
+
+  Args:
+    previous_value:  A tensor.
+    x: A tensor.
+    name: name of variable scope; defaults to zero_add.
+    reuse: reuse scope.
+
+  Returns:
+    previous_value + gamma * x.
+  """
+  with tf.variable_scope(
+      name, default_name="zero_add", reuse=reuse):
     gamma = tf.get_variable(
-        "gamma", [None], initializer=tf.zeros_initializer())
+        "gamma", (), initializer=tf.zeros_initializer())
     return previous_value + gamma*x
 
 
