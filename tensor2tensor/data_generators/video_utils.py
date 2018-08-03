fad326564c7dfc3155d0e250a4bde34fc18ed7f5
cbockman <c.bockman@gmail.com>
2018-08-03 14:32:58 -0700

    Revert "update my branch further"

--- tensor2tensor/data_generators/video_utils.py
+++ tensor2tensor/data_generators/video_utils.py
@@ -140,7 +140,7 @@ class VideoProblem(problem.Problem):
 
   def preprocess_example(self, example, mode, hparams):
     """Runtime preprocessing, e.g., resize example["frame"]."""
-    if getattr(hparams, "preprocess_resize_frames", None) is not None:
+    if hparams.preprocess_resize_frames is not None:
       example["frame"] = tf.image.resize_images(
           example["frame"],
           hparams.preprocess_resize_frames,
