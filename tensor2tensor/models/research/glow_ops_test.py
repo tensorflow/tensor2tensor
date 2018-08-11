e6caaf2afe0e2dda58aa43483ea2d214dcdc4be1
Ryan Sepassi <rsepassi@google.com>
2018-08-10 17:30:24 -0700

    Fix description
    PiperOrigin-RevId: 208295334

--- tensor2tensor/models/research/glow_ops_test.py
+++ tensor2tensor/models/research/glow_ops_test.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Tests for google3.third_party.py.tensor2tensor.models.research.glow_ops."""
+"""Tests for tensor2tensor.models.research.glow_ops."""
 
 from __future__ import absolute_import
 from __future__ import division
