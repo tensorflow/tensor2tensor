allow mtf.slicewise with no gradient.  Fix bug in mtf.Print.
PiperOrigin-RevId: 208376978

tensor2tensor/mesh_tensorflow/mesh_tensorflow.py
@@ -1119,6 +1119,7 @@ class SlicewiseOperation(Operation):
                output_shape,
                output_dtype,
                splittable_dims,
+               has_gradient=True,
                grad_function=None,
                name=None):
     """Create a SlicewiseOperation.
@@ -1135,14 +1136,20 @@ class SlicewiseOperation(Operation):
       output_shape: a Shape
       output_dtype: a dtype
       splittable_dims: a list of Dimensions which are ok to split
-      grad_function: an optional python function
+      has_gradient: a boolean
+      grad_function: an optional python function. Default to using tf.gradients
       name: an optional string
     """
     super(SlicewiseOperation, self).__init__(inputs, name=name or "slicewise")
     self._tf_fn = tf_fn
     self._outputs = [Tensor(self, output_shape, output_dtype)]
     self._splittable_dims = splittable_dims
+    self._has_gradient = has_gradient
     self._grad_function = grad_function
+
+  @property
+  def has_gradient(self):
+    return self._has_gradient
 
   def gradient(self, grad_ys):
     if self._grad_function is not None:
@@ -1168,6 +1175,7 @@ def slicewise(tf_fn,
               output_shape=None,
               output_dtype=None,
               splittable_dims=None,
+              has_gradient=True,
               grad_function=None,
               name=None):
   """Slice-wise call to any tensorflow function.
@@ -1182,7 +1190,8 @@ def slicewise(tf_fn,
     output_shape: a Shape
     output_dtype: a dtype
     splittable_dims: a list of Dimensions which are ok to split
-    grad_function: an optional gradients function
+    has_gradient: a boolean
+    grad_function: an optional gradients function.  If None, use tf gradient.
     name: an optional string
 
   Returns:
@@ -1194,6 +1203,7 @@ def slicewise(tf_fn,
       convert_to_shape(output_shape) or xs[0].shape,
       output_dtype or xs[0].dtype,
       splittable_dims,
+      has_gradient,
       grad_function,
       name=name).outputs[0]
 
@@ -2253,8 +2263,8 @@ class PrintOperation(Operation):
         self.outputs[0],
         lowering.mesh_impl(self).Print(
             lowering.tensors[self.inputs[0]],
-            [lowering.tensors[d] for d in self._data], self._message,
-            **self._kwargs))
+            [lowering.tensors[d].to_laid_out_tensor() for d in self._data],
+            self._message, **self._kwargs))
 
   def gradient(self, grad_ys):
     return grad_ys