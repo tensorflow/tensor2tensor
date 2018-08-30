37625c7e24b965cf27b15f1c4f8dcd7b20d07405
Dustin Tran <trandustin@google.com>
2018-08-30 15:13:36 -0700

    Clean up ImageChannelCompressModality.
    PiperOrigin-RevId: 210989363

--- tensor2tensor/layers/modalities.py
+++ tensor2tensor/layers/modalities.py
@@ -303,18 +303,19 @@ class ImageChannelCompressModality(modality.Modality):
     return 3
 
   def bottom_compress(self, inputs, name="bottom"):
-    """Transform input from data space to model space.
+    """Compresses channel-wise input pixels into whole pixel representions.
 
-    Perform conversion of RGB pixel values to a real number in the range -1 to 1
-    and combine channel values for each pixel to form a representation of
-    size image_length x image_length dims.
+    Perform conversion of RGB pixel values to a real number in the range -1 to
+    1. This combines pixel channels to form a representation of shape
+    [img_len, img_len].
 
     Args:
-      inputs: A Tensor representing RGB pixel intensities as integers.
-        [batch, ...]
+      inputs: Tensor representing RGB pixel intensities as integers, of shape
+        [batch, img_len, img_len, channels].
       name: string, scope.
+
     Returns:
-      body_input: A Tensor with shape [batch, ?, ?, body_input_depth].
+      body_input: Tensor of shape [batch, img_len, img_len, body_input_depth].
     """
     with tf.variable_scope(name):
       inputs = tf.to_float(inputs)
@@ -325,19 +326,22 @@ class ImageChannelCompressModality(modality.Modality):
             common_layers.tpu_safe_image_summary(inputs),
             max_outputs=2)
       inputs = common_layers.convert_rgb_to_symmetric_real(inputs)
-      ishape = common_layers.shape_list(inputs)
-      inputs = tf.reshape(inputs, [-1, ishape[1], ishape[2] * ishape[3], 1])
-      inputs.set_shape([None, None, None, 1])
-      # We compress RGB intensities for each pixel using a conv.
-      x = tf.layers.conv2d(
+
+      # Reshape inputs to apply convolutions across [img_len, img_len*channels].
+      inputs_shape = common_layers.shape_list(inputs)
+      inputs = tf.reshape(
+          inputs, [-1, inputs_shape[1], inputs_shape[2] * inputs_shape[3], 1])
+
+      # Compress RGB intensities for each pixel using a convolution.
+      outputs = tf.layers.conv2d(
           inputs,
-          self._body_input_depth, (1, self.num_channels),
+          self._body_input_depth,
+          kernel_size=(1, self.num_channels),
           padding="VALID",
           strides=(1, self.num_channels),
           activation=tf.nn.relu,
           name="conv_input")
-      x.set_shape([None, None, None, self._body_input_depth])
-      return x
+      return outputs
 
   def bottom(self, x):
     return self.bottom_compress(x, "input_bottom")
@@ -346,24 +350,36 @@ class ImageChannelCompressModality(modality.Modality):
     return self.bottom_compress(x, "output_bottom")
 
   def top(self, body_output, _):
+    """Transforms body output to return logits.
+
+    Args:
+      body_output: Tensor of shape [batch, img_len, img_len, depth].
+
+    Returns:
+      Tensor of shape [batch, img_len, img_len, channels, top_dimensionality].
+    """
     with tf.variable_scope(self.name):
-      hidden_dim = self._model_hparams.hidden_size
+      hidden_size = self._model_hparams.hidden_size
       img_len = self._model_hparams.img_len
       channels = self.num_channels  # RGB
       batch = common_layers.shape_list(body_output)[0]
       x = tf.layers.conv2d(
           body_output,
-          hidden_dim * channels, (1, 1),
+          hidden_size * channels,
+          kernel_size=(1, 1),
           strides=(1, 1),
           padding="VALID",
           activation=tf.nn.relu,
           name="decompress_conv")
-      x = tf.reshape(x, [batch, img_len, img_len * channels, hidden_dim])
+      x = tf.reshape(x, [batch, img_len, img_len * channels, hidden_size])
       x = common_layers.layer_preprocess(x, self._model_hparams)
-      x = tf.layers.dense(
-          x, 256, use_bias=True, activation=None, name="output_conv")
-      x = tf.reshape(x,
-                     [-1, img_len, img_len, channels, self.top_dimensionality])
+      x = tf.layers.dense(x,
+                          self.top_dimensionality,
+                          use_bias=True,
+                          activation=None,
+                          name="output_conv")
+      x = tf.reshape(
+          x, [batch, img_len, img_len, channels, self.top_dimensionality])
       return x
 
 
