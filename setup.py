8c02799bbc6270ea97ca4c64444bb93ea97f80a1
Ryan Sepassi <rsepassi@google.com>
2018-09-05 12:03:12 -0700

    Add matplotlib to [tests] and ignore some tests in travis
    PiperOrigin-RevId: 211673360

--- setup.py
+++ setup.py
@@ -52,7 +52,8 @@ setup(
         'tensorflow': ['tensorflow>=1.9.0'],
         'tensorflow_gpu': ['tensorflow-gpu>=1.9.0'],
         'tests': [
-            'absl-py', 'pytest', 'mock', 'pylint', 'jupyter', 'gsutil'
+            'absl-py', 'pytest', 'mock', 'pylint', 'jupyter', 'gsutil',
+            'matplotlib',
             # Need atari extras for Travis tests, but because gym is already in
             # install_requires, pip skips the atari extras, so we instead do an
             # explicit pip install gym[atari] for the tests.
