32d227325a984a0045c30ebd4abd909db6695566
piotrmilos <piotr.milos@codilime.com>
2018-09-08 17:46:51 -0700

    some debug information

--- tensor2tensor/rl/model_rl_experiment_player.py
+++ tensor2tensor/rl/model_rl_experiment_player.py
@@ -3,12 +3,11 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import contextlib
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
 import copy
-import datetime
-import math
 import os
-import time
 from gym.spaces import Box
 import numpy as np
 
@@ -26,11 +25,53 @@ from tensor2tensor.rl.envs.utils import get_policy
 from tensor2tensor.utils import registry
 from tensor2tensor.utils import trainer_lib
 from tensor2tensor.rl.trainer_model_based import FLAGS, setup_directories, temporary_flags
-
+from gym.utils import play
 import tensorflow as tf
 
 
 HP_SCOPES = ["loop", "model", "ppo"]
+
+_font = None
+FONT_SIZE = 20
+
+def _get_font():
+  global _font
+  if _font is None:
+    #weirdness due to various working dirs
+    FONT_PATHS = ["tensor-2-tensor-with-mrunner/tensor-2-tensor-with-mrunner/deepsense_experiments/Xerox Serif Narrow.ttf",
+                  "tensor-2-tensor-with-mrunner/deepsense_experiments/Xerox Serif Narrow.ttf",
+                  "deepsense_experiments/Xerox Serif Narrow.ttf"]
+
+    for path in FONT_PATHS:
+      try:
+        _font = ImageFont.truetype(path, FONT_SIZE)
+        return _font
+      except:
+        pass
+
+
+def _assert_image(img):
+  if isinstance(img, np.ndarray):
+    img = Image.fromarray(np.ndarray.astype(img, np.uint8))
+  return img
+
+
+def write_on_image(img, text="", positon=(0,0), color=(255,255,255)):
+  img = _assert_image(img)
+  if text=="":
+    return img
+  draw = ImageDraw.Draw(img)
+  font = _get_font()
+  draw.text(positon, text, color, font=font)
+
+  return img
+
+def concatenate_images(*imgs, axis=1):
+  imgs = [_assert_image(img) for img in imgs]
+  imgs_np = [np.array(img) for img in imgs]
+  concatenated_im_np = np.concatenate(imgs_np, axis=axis)
+
+  return _assert_image(concatenated_im_np)
 
 
 def train_agent(problem_name, agent_model_dir,
@@ -76,11 +117,13 @@ def train_agent(problem_name, agent_model_dir,
     sess = tf.Session()
     env = DebugBatchEnv(ppo_hparams, sess)
     sess.run(tf.global_variables_initializer())
-    env.initialize()
+    # env.initialize()
 
-    key_mapping = {(ord('q'),):1, (ord('a'),):2}
-    from gym.utils import play
-    play.play(env, zoom=4, fps=40, keys_to_action=key_mapping)
+    key_mapping = {(): 100, (ord('q'),):1, (ord('a'),):2,
+                   (ord('r'),):101,
+                   (ord('p'),):102}
+
+    play.play(env, zoom=2, fps=10, keys_to_action=key_mapping)
 
 from gym.core import Env
 
@@ -96,47 +139,56 @@ class DebugBatchEnv(Env):
     self.action_space = Discrete(6)
     self.observation_space = Box(low=0, high=255, shape=(210, 160, 3), dtype=np.uint8)
 
-    batch_env = batch_env_factory(hparams)
+    # batch_env = batch_env_factory(hparams)
 
     self.action = tf.placeholder(shape=(1,), dtype=tf.int32)
 
-    self.reward, self.done = batch_env.simulate(self.action)
-    self.observation = batch_env.observ
-    self.reset_op = batch_env.reset(tf.constant([0], dtype=tf.int32))
+    # self.reward, self.done = batch_env.simulate(self.action)
+    # self.observation = batch_env.observ
+    # self.reset_op = batch_env.reset(tf.constant([0], dtype=tf.int32))
 
     environment_wrappers = hparams.environment_spec.wrappers
     wrappers = copy.copy(environment_wrappers) if environment_wrappers else []
 
-    to_initialize = [batch_env]
-    for w in wrappers:
-      batch_env = w[0](batch_env, **w[1])
-      to_initialize.append(batch_env)
+    # to_initialize = [batch_env]
+    # for w in wrappers:
+    #   batch_env = w[0](batch_env, **w[1])
+    #   to_initialize.append(batch_env)
+    #
+    # def initialization_lambda():
+    #   for batch_env in to_initialize:
+    #     batch_env.initialize(sess)
 
-    def initialization_lambda():
-      for batch_env in to_initialize:
-        batch_env.initialize(sess)
+    # self.initialize = initialization_lambda
 
-    self.initialize = initialization_lambda
+    # obs_copy = batch_env.observ + 0
 
-    obs_copy = batch_env.observ + 0
-
-    actor_critic = get_policy(tf.expand_dims(obs_copy, 0), hparams)
-    self.policy_probs = actor_critic.policy.probs[0, 0, :]
-    self.value = actor_critic.value[0, :]
+    # actor_critic = get_policy(tf.expand_dims(obs_copy, 0), hparams)
+    # self.policy_probs = actor_critic.policy.probs[0, 0, :]
+    # self.value = actor_critic.value[0, :]
     self._tmp = 1
+    self.res = None
 
   def render(self, mode='human'):
     raise NotImplemented()
 
   def reset(self):
-    observ = self.sess.run(self.reset_op)
+    # observ = self.sess.run(self.reset_op)
+    self._tmp = 0
+    _observ = np.ones(shape=(210, 160, 3), dtype=np.uint8) * 10 * self._tmp
+    _observ[0, 0, 0] = 0
+    _observ[0, 0, 1] = 255
+    self.res = (_observ, 0, False, "a", "b")
+    observ = self._augment_observation()
     return observ
 
 
   def _step_fake(self, action):
 
-    print("Action:{}".format(action))
     observ = np.ones(shape=(210, 160, 3), dtype=np.uint8)*10*self._tmp
+    observ[0, 0, 0] = 0
+    observ[0, 0, 1] = 255
+
     self._tmp += 1
     if self._tmp>20:
       self._tmp = 0
@@ -155,8 +207,36 @@ class DebugBatchEnv(Env):
 
     return observ[0, ...], rew[0, ...], done[0, ...], probs, vf
 
+  def _augment_observation(self):
+    _observ, rew, probs, probs, vf = self.res
+    info_pane = np.zeros_like(_observ)
+    write_on_image(info_pane, "Policy:{}".format(probs))
+    write_on_image(info_pane, "Value function:{}".format(vf), positon=(0, 10))
+    write_on_image(info_pane, "Rew:{}".format(rew), positon=(0, 20))
+
   def step(self, action):
-    observ, rew, done, probs, vf = self._step_fake(action)
+    #Special codes
+    if action==100:
+      #Skip action
+      observ, rew, done, _, _ = self.res
+      return observ, rew, done, {}
+
+    if action == 101:
+      #reset
+      observ, rew, _ = self.res
+      return observ, rew, True, {}
+
+    if action == 102:
+      #play
+      raise NotImplemented()
+
+    #standard codes
+    _observ, rew, done, probs, vf = self._step_fake(action)
+    self.res = (_observ, rew, done, probs, vf)
+
+    observ = self._augment_observation()
+
+
     return observ, rew, done, {"probs": probs, "vf": vf}
 
 
@@ -181,34 +261,14 @@ def training_loop(hparams, output_dir, report_fn=None, report_metric=None):
     game_with_mode = hparams.game
   # Problems
   if using_autoencoder:
-    problem_name = (
-        "gym_discrete_problem_with_agent_on_%s_with_autoencoder"
-        % game_with_mode)
-    world_model_problem = (
-        "gym_discrete_problem_with_agent_on_%s_autoencoded" % game_with_mode)
     simulated_problem_name = (
         "gym_simulated_discrete_problem_with_agent_on_%s_autoencoded"
         % game_with_mode)
   else:
-    problem_name = ("gym_discrete_problem_with_agent_on_%s" % game_with_mode)
-    world_model_problem = problem_name
     simulated_problem_name = ("gym_simulated_discrete_problem_with_agent_on_%s"
                               % game_with_mode)
-    if problem_name not in registry.list_problems():
-      tf.logging.info("Game Problem %s not found; dynamically registering",
-                      problem_name)
-      gym_problems_specs.create_problems_for_game(hparams.game,
-                                                  game_mode="Deterministic-v4")
 
-  # Autoencoder model dir
-  autoencoder_model_dir = directories.get("autoencoder")
-
-  # Per-epoch state
-  epoch_metrics = []
   epoch_data_dirs = []
-
-
-  # Collect data from the real environment with random policy
   data_dir = os.path.join(directories["data"], "random")
   epoch_data_dirs.append(data_dir)
 
