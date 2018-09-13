3a26ad7b5c24e025a92e5ebaa88c19e4b63a33a3
Dustin Tran <trandustin@google.com>
2018-09-13 11:21:58 -0700

    Correct trainer_lib_test setup and lower its BUILD deps.
    PiperOrigin-RevId: 212845915

--- tensor2tensor/utils/trainer_lib_test.py
+++ tensor2tensor/utils/trainer_lib_test.py
@@ -18,9 +18,9 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from tensor2tensor import models  # pylint: disable=unused-import
 from tensor2tensor.data_generators import algorithmic
 from tensor2tensor.data_generators import problem as problem_lib
+from tensor2tensor.models import transformer  # pylint: disable=unused-import
 from tensor2tensor.utils import registry
 from tensor2tensor.utils import trainer_lib
 import tensorflow as tf
@@ -78,16 +78,16 @@ class TrainerLibTest(tf.test.TestCase):
       self.assertEqual(loss_val.shape, tuple())
 
   def testMultipleTargetModalities(self):
-    # HParams
+    # Use existing hparams and override target modality.
     hparams = trainer_lib.create_hparams(
         "transformer_tiny", data_dir=algorithmic.TinyAlgo.data_dir,
         problem_name="tiny_algo")
-    tm = hparams.problem.get_hparams().target_modality
-    hparams.problem.get_hparams().target_modality = {
-        "targets": tm,
-        "A": tm,
-        "B": tm
+    hparams.problem_hparams.target_modality = {
+        "targets": hparams.problem_hparams.target_modality,
+        "A": hparams.problem_hparams.target_modality,
+        "B": hparams.problem_hparams.target_modality
     }
+    hparams.problem._hparams = hparams.problem_hparams
 
     # Dataset
     problem = hparams.problem
