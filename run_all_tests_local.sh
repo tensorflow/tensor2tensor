c973a7b26ced4781e52a9f4b26c2503c6c0ee97e
rllin-fathom <randall@fathomhealth.co>
2018-10-17 22:00:03 +0000

    skip and explain

--- run_all_tests_local.sh
+++ run_all_tests_local.sh
@@ -33,6 +33,11 @@ source ~/diseaseTools/scripts/vm_setup/dev_config/.bashrc_aliases_fathom
 # * skip gym_problems (gym not in our image)
 # * skip checkpoint_compatibility_test.py (no tqdm; undo this at some point and just install tqdm in image)
 # * skip tensor2tensor/models/research/next_frame_test.py b/c not working but clearly experimental on t2t side
+# * skip glow_test which requires cifar dataset
+#     https://github.com/tensorflow/tensor2tensor/blob/3f43417310101859f95b74587ffc3686714cc58a/oss_scripts/oss_tests.sh#L71
+# * skip common_video_test.py::CommonVideoTest::testGifSummary because of ffmpeg dependency
+# * skip tensor2tensor/data_generators/image_utils_test.py because of matplotlib dependency
+# * skip tensor2tensor/layers/common_video_test.py because of ffmpeg dependency
 
 dki gcr.io/fathom-containers/t2t_test python3 -m pytest -vv \
        --ignore=/usr/src/app/api-flask/ \
@@ -49,10 +54,10 @@ dki gcr.io/fathom-containers/t2t_test python3 -m pytest -vv \
        --ignore=/usr/src/t2t/tensor2tensor/models/research/next_frame_test.py \
        --ignore=/usr/src/t2t/tensor2tensor/rl/trainer_model_based_stochastic_test.py \
        --ignore=/usr/src/t2t/tensor2tensor/rl/trainer_model_based_sv2p_test.py \
-       # skip glow_test which requires cifar dataset
-       # https://github.com/tensorflow/tensor2tensor/blob/3f43417310101859f95b74587ffc3686714cc58a/oss_scripts/oss_tests.sh#L71
        --ignore=/usr/src/t2t/tensor2tensor/models/research/glow_test.py \
        --deselect=/usr/src/t2t/tensor2tensor/layers/common_video_test.py::CommonVideoTest::testGifSummary \
+       --ignore=/usr/src/t2t/tensor2tensor/data_generators/image_utils_test.py \
+       --ignore=/usr/src/t2t/tensor2tensor/layers/common_video_test.py \
        --junitxml=/usr/src/t2t/test_results/pytest/unittests.xml \
        /usr/src/t2t/tensor2tensor/
 
