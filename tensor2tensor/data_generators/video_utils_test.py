df07bcf000631fd539e9a6eb46ea0b8ccd348c47
Afroz Mohiuddin <afrozm@google.com>
2018-10-26 09:29:18 -0700

    Rename function to pass OSS lint.
    PiperOrigin-RevId: 218865306

--- tensor2tensor/data_generators/video_utils_test.py
+++ tensor2tensor/data_generators/video_utils_test.py
@@ -29,7 +29,7 @@ import tensorflow as tf
 
 class VideoUtilsTest(tf.test.TestCase):
 
-  def getPredictions(self):
+  def get_predictions(self):
     rng = np.random.RandomState(0)
     # num_samples=4
     inputs = rng.randint(0, 255, (4, 2, 64, 64, 3))
@@ -46,7 +46,7 @@ class VideoUtilsTest(tf.test.TestCase):
     return predictions, problem
 
   def testDecodeInMemoryTrue(self):
-    predictions, problem = self.getPredictions()
+    predictions, problem = self.get_predictions()
     decode_hparams = decoding.decode_hparams()
     decode_hparams.decode_in_memory = True
     decode_hooks = decoding.DecodeHookArgs(
