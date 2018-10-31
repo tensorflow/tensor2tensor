36596aa92b2da1be6a7ebcbcc06e34c326ac1604
Giovanni Campagna <scampa.giovanni@gmail.com>
2018-10-31 14:59:20 -0700

    trainer_lib: add problem hparams after parsing the overrides (#1053)
    t2t-trainer passes problem_name=None to create_hparams(), and
    adds the problem hparams afterwards when creating the experiment.
    t2t-decoder instead relies fully on create_hparams()
    This inconsistency causes t2t-trainer and t2t-decoder to pass
    different sets of model_hparams to Problem.hparams() (before and
    after overrides respectively), which is problematic.
    
    Always letting the problem see the final version of the model
    hparams is more useful (in case the problem hparams depend on
    the model), so make sure the problem is added after the overrides.

--- tensor2tensor/utils/trainer_lib.py
+++ tensor2tensor/utils/trainer_lib.py
@@ -96,12 +96,12 @@ def create_hparams(hparams_set,
     hparams = _create_hparams_from_json(hparams_path, hparams)
   if data_dir:
     hparams.add_hparam("data_dir", data_dir)
-  if problem_name:
-    add_problem_hparams(hparams, problem_name)
   if hparams_overrides_str:
     tf.logging.info("Overriding hparams in %s with %s", hparams_set,
                     hparams_overrides_str)
     hparams = hparams.parse(hparams_overrides_str)
+  if problem_name:
+    add_problem_hparams(hparams, problem_name)
   return hparams
 
 
