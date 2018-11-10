one more bug(?) fix. this is to allow backproping through previously predicted frame after conversion from float to int.
PiperOrigin-RevId: 220889018

tensor2tensor/models/video/base.py
@@ -391,7 +391,7 @@ class NextFrameBase(t2t_model.T2TModel):
       sampled_frame = common_layers.standardize_images(sampled_frame)
     else:
       x = common_layers.convert_real_to_rgb(pred_frame)
-      x = tf.cast(x, tf.uint8)
+      x = x - tf.stop_gradient(x + tf.round(x))
       x = common_layers.convert_rgb_to_real(x)
       return x
     return sampled_frame