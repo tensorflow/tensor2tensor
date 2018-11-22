e9554fc27f2d59fab0e37d35fecfb3bbe0ee523f
Mohammad Babaeizadeh <mbz@google.com>
2018-11-21 16:33:18 -0800

    reward model for SV2P from intermediate layers.
    PiperOrigin-RevId: 222475449

--- tensor2tensor/models/video/sv2p.py
+++ tensor2tensor/models/video/sv2p.py
@@ -150,19 +150,43 @@ class NextFrameSv2p(base.NextFrameBase, base_vae.NextFrameBaseVae):
       return self.reward_prediction_basic(*args, **kwargs)
     elif model == "big":
       return self.reward_prediction_big(*args, **kwargs)
+    elif model == "mid":
+      return self.reward_prediction_mid(*args, **kwargs)
     else:
       raise ValueError("Unknown reward model %s" % model)
 
-  def reward_prediction_basic(self, input_images, input_reward, action, latent):
-    del input_reward, action, latent
+  def reward_prediction_basic(
+      self, input_images, input_reward, action, latent, mid_outputs):
+    del input_reward, action, latent, mid_outputs
     x = input_images
     x = tf.reduce_mean(x, axis=[1, 2], keepdims=True)
     x = tfl.dense(x, 128, activation=tf.nn.relu, name="reward_pred")
     x = tf.expand_dims(x, axis=3)
     return x
 
-  def reward_prediction_big(self, input_images, input_reward, action, latent):
+  def reward_prediction_mid(
+      self, input_images, input_reward, action, latent, mid_outputs):
+    """Builds a reward prediction network from intermediate layers."""
+    encoded = []
+    for i, output in enumerate(mid_outputs):
+      enc = output
+      enc = tfl.conv2d(enc, 64, [3, 3], strides=(1, 1), activation=tf.nn.relu)
+      enc = tfl.conv2d(enc, 32, [3, 3], strides=(2, 2), activation=tf.nn.relu)
+      enc = tfl.conv2d(enc, 16, [3, 3], strides=(2, 2), activation=tf.nn.relu)
+      enc = tfl.flatten(enc)
+      enc = tfl.dense(enc, 64, activation=tf.nn.relu, name="rew_enc_%d" % i)
+      encoded.append(enc)
+    x = encoded
+    x = tf.stack(x, axis=1)
+    x = tfl.flatten(x)
+    x = tfl.dense(x, 256, activation=tf.nn.relu, name="rew_dense1")
+    x = tfl.dense(x, 128, activation=tf.nn.relu, name="rew_dense2")
+    return x
+
+  def reward_prediction_big(
+      self, input_images, input_reward, action, latent, mid_outputs):
     """Builds a reward prediction network."""
+    del mid_outputs
     conv_size = self.tinyify([32, 32, 16, 8])
 
     with tf.variable_scope("reward_pred", reuse=tf.AUTO_REUSE):
@@ -326,7 +350,8 @@ class NextFrameSv2p(base.NextFrameBase, base_vae.NextFrameBaseVae):
         output = tf.layers.dense(
             output, self.hparams.problem.num_channels * 256, name="logits")
 
-      return output, lstm_state
+      mid_outputs = [enc0, enc1, enc4, enc5, enc6]
+      return output, lstm_state, mid_outputs
 
   def video_features(
       self, all_frames, all_actions, all_rewards, all_raw_frames):
@@ -351,14 +376,14 @@ class NextFrameSv2p(base.NextFrameBase, base_vae.NextFrameBaseVae):
       if latent_mean is not None:
         extra_loss = self.get_extra_loss([latent_mean], [latent_std])
 
-    pred_image, internal_states = self.construct_predictive_tower(
+    pred_image, internal_states, mid_outputs = self.construct_predictive_tower(
         frames, None, actions, internal_states, latent)
 
     if not self.has_rewards:
       return pred_image, None, extra_loss, internal_states
 
     pred_reward = self.reward_prediction(
-        pred_image, actions, rewards, latent)
+        pred_image, actions, rewards, latent, mid_outputs)
     return pred_image, pred_reward, extra_loss, internal_states
 
 
@@ -418,7 +443,7 @@ class NextFrameSv2pDiscrete(NextFrameSv2p):
     extra_loss = 0.0
     latent = self.simple_discrete_latent_tower(frames, target_frame)
 
-    pred_image, internal_states = self.construct_predictive_tower(
+    pred_image, internal_states, _ = self.construct_predictive_tower(
         frames, None, actions, internal_states, latent, True)
 
     if not self.has_rewards:
@@ -549,7 +574,7 @@ class NextFrameSv2pLegacy(NextFrameSv2p):
           done_warm_start, groundtruth_items, generated_items, ss_func)
 
       # Prediction
-      pred_image, lstm_states = self.construct_predictive_tower(
+      pred_image, lstm_states, _ = self.construct_predictive_tower(
           input_image, None, action, lstm_states, latent)
 
       if self.hparams.reward_prediction:
@@ -760,7 +785,7 @@ class NextFrameSv2pTwoFrames(NextFrameSv2pLegacy):
       latent_stds.append(latent_std)
 
       # Prediction
-      pred_image, lstm_state = self.construct_predictive_tower(
+      pred_image, lstm_state, _ = self.construct_predictive_tower(
           input_image, input_reward, action, lstm_state, latent)
 
       if self.hparams.reward_prediction:
