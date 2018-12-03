f58a4854e3e03b88748f2d8ed052d4cc7cc22e76
cclauss <cclauss@bluewin.ch>
2018-12-04 00:32:18 +0100

    Undefined name: xrange() was removed in Python 3 (#1258)

--- tensor2tensor/models/research/vqa_self_attention.py
+++ tensor2tensor/models/research/vqa_self_attention.py
@@ -19,6 +19,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from six.moves import xrange
+
 from tensor2tensor.layers import common_attention
 from tensor2tensor.layers import common_hparams
 from tensor2tensor.layers import common_layers
