768dc796898c7d0624687688aec91fdfa4dd9718
wushangyu <534427411@qq.com>
2018-12-05 11:04:23 +0800

    update metrics test

--- tensor2tensor/utils/metrics_test.py
+++ tensor2tensor/utils/metrics_test.py
@@ -325,12 +325,10 @@ class MetricsTest(tf.test.TestCase):
     
     expected = np.corrcoef(np.squeeze(predictions), np.squeeze(targets))[0][1]
     with self.test_session() as session:
-      pearson, _ = metrics.pearson_correlation_coefficient(
-          tf.constant(predictions, dtype=tf.float32), 
-          tf.constant(targets, dtype=tf.float32))
-      a = tf.reduce_mean(pearson)
-      session.run(tf.global_variables_initializer())
-      actual = session.run(a)
+      actual, w = session.run(
+          metrics.pearson_correlation_coefficient(
+              tf.constant(predictions, dtype=tf.float32), 
+              tf.constant(targets, dtype=tf.float32)))
     self.assertAlmostEqual(actual, expected)
 
 if __name__ == '__main__':
