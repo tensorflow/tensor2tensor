51c9e61b1da831561f5b27857bc98c8a50d59aca
Piotr Kozakowski <kozak000@gmail.com>
2019-01-09 17:03:13 +0000

    Quick fix for stochastic discrete param sharing

--- tensor2tensor/rl/ppo_learner.py
+++ tensor2tensor/rl/ppo_learner.py
@@ -134,7 +134,8 @@ class PPOLearner(PolicyLearner):
             sampling_temp=sampling_temp,
         )
         model_saver = tf.train.Saver(
-            tf.global_variables(hparams.policy_network + "/.*")
+            tf.global_variables(hparams.policy_network + "/.*") +
+            tf.global_variables("clean_scope.*")  # Needed for the SD model.
         )
 
         with tf.Session() as sess:
@@ -198,6 +199,7 @@ def _run_train(ppo_hparams,
   model_saver = tf.train.Saver(
       tf.global_variables(ppo_hparams.policy_network + "/.*") +
       tf.global_variables("training/" + ppo_hparams.policy_network + "/.*") +
+      tf.global_variables("clean_scope.*") +  # Needed for the SD model.
       tf.global_variables("global_step") +
       tf.global_variables("losses_avg.*") +
       tf.global_variables("train_stats.*")
