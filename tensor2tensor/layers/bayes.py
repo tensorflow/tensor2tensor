5514b059122370f9d196ac253e6680ee8eac732f
Mike Dusenberry <dusenberrymw@google.com>
2019-01-15 12:03:06 -0800

    Improve the initialization scheme for the Bayesian Layers.
    This updates the TrainableNormal initialization to a Normal with mean `0` and
    standard deviation `sqrt(1. / fan_in)`, where `fan_in` is the input
    dimensionality of the layer. Additionally, a small amount of noise is added to
    each to break symmetry.  This also includes the He et al. 2015 variant that
    uses the standard deviation `sqrt(2. / fan_in)` for layers that are followed
    by ReLU nonlinearities, and the Glorot 2010 variant that uses `sqrt(2. / fan_in
    + fan_out)`, where `fan_out` is the output dimensionality of the layer.
    
    PiperOrigin-RevId: 229414516

--- tensor2tensor/layers/bayes.py
+++ tensor2tensor/layers/bayes.py
@@ -19,6 +19,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import math
+
 import tensorflow as tf
 import tensorflow_probability as tfp
 
@@ -125,6 +127,99 @@ class LinearKernel(object):
     }
 
 
+# From `tensorflow/python/ops/init_ops.py`
+def _compute_fans(shape):
+  """Computes the number of input and output units for a weight shape.
+
+  Args:
+    shape: Integer shape tuple or TF tensor shape.
+
+  Returns:
+    A tuple of scalars (fan_in, fan_out).
+  """
+  if len(shape) < 1:  # Just to avoid errors for constants.
+    fan_in = fan_out = 1
+  elif len(shape) == 1:
+    fan_in = fan_out = shape[0]
+  elif len(shape) == 2:
+    fan_in = shape[0]
+    fan_out = shape[1]
+  else:
+    # Assuming convolution kernels (2D, 3D, or more).
+    # kernel shape: (..., input_depth, depth)
+    receptive_field_size = 1.
+    for dim in shape[:-2]:
+      receptive_field_size *= dim
+    fan_in = shape[-2] * receptive_field_size
+    fan_out = shape[-1] * receptive_field_size
+  return fan_in, fan_out
+
+
+class ScaledNormalStdDev(tf.keras.initializers.VarianceScaling):
+  """Initializer capable of adapting its scale to the shape of weights tensors.
+
+  This initializes the standard deviation parameter of a Trainable Normal
+  distribution with a scale based on the shape of the weights tensor.
+  Additionally, A small amount of noise will be added to break weigh symmetry.
+
+  With `distribution="truncated_normal" or "untruncated_normal"`, the standard
+  deviation (after truncation, if used) is `stddev = sqrt(scale / n)`, where n
+  is:
+    - number of input units in the weight tensor, if mode = "fan_in"
+    - number of output units, if mode = "fan_out"
+    - average of the numbers of input and output units, if mode = "fan_avg"
+
+  Args:
+    scale: Scaling factor (positive float).
+    mode: One of "fan_in", "fan_out", "fan_avg".
+    distribution: Random distribution to use. One of "truncated_normal", or
+      "untruncated_normal".
+    seed: A Python integer. Used to create random seeds. See
+      `tf.set_random_seed`
+      for behavior.
+    dtype: The data type. Only floating point types are supported.
+
+  Raises:
+    ValueError: In case of an invalid value for the "scale", mode" or
+      "distribution" arguments.
+  """
+
+  def __init__(self,
+               scale=1.0,
+               mode='fan_in',
+               distribution='untruncated_normal',
+               seed=None,
+               dtype=tf.float32):
+    distribution = distribution.lower()
+    if distribution not in {'truncated_normal', 'untruncated_normal'}:
+      raise ValueError('Invalid `distribution` argument:', distribution)
+    super(ScaledNormalStdDev, self).__init__(scale=scale, mode=mode,
+                                             distribution=distribution,
+                                             seed=seed, dtype=dtype)
+
+  def __call__(self, shape, dtype=None, partition_info=None):
+    if dtype is None:
+      dtype = self.dtype
+    scale = self.scale
+    scale_shape = shape
+    if partition_info is not None:
+      scale_shape = partition_info.full_shape
+    fan_in, fan_out = _compute_fans(scale_shape)
+    if self.mode == 'fan_in':
+      scale /= max(1., fan_in)
+    elif self.mode == 'fan_out':
+      scale /= max(1., fan_out)
+    else:
+      scale /= max(1., (fan_in + fan_out) / 2.)
+    if self.distribution == 'truncated_normal':
+      # constant from scipy.stats.truncnorm.std(a=-2, b=2, loc=0., scale=1.)
+      stddev = math.sqrt(scale) / .87962566103423978
+    else:  # self.distribution == 'untruncated_normal':
+      stddev = math.sqrt(scale)
+    return tf.random.truncated_normal(shape, mean=stddev, stddev=stddev*0.1,
+                                      dtype=dtype)
+
+
 # TODO(dusenberrymw): Restructure the implementation of a trainable initializer
 # such that callers do not need to have type-conditional logic.
 class TrainableInitializer(tf.keras.initializers.Initializer):
@@ -146,9 +241,9 @@ class TrainableNormal(TrainableInitializer):
   """Random normal op as an initializer with trainable mean and stddev."""
 
   def __init__(self,
-               mean_initializer=tf.random_normal_initializer(stddev=0.1),
-               stddev_initializer=tf.random_uniform_initializer(
-                   minval=1e-5, maxval=0.1),
+               mean_initializer=tf.keras.initializers.truncated_normal(
+                   stddev=1e-5),
+               stddev_initializer=ScaledNormalStdDev(),
                mean_regularizer=None,
                stddev_regularizer=None,
                mean_constraint=None,
@@ -221,8 +316,70 @@ class TrainableNormal(TrainableInitializer):
     }
 
 
+class TrainableHeNormal(TrainableNormal):
+  """Trainable normal initialized per He et al. 2015, given a ReLU nonlinearity.
+
+  The distribution is initialized to a Normal scaled by `sqrt(2 / fan_in)`,
+  where `fan_in` is the number of input units. A ReLU nonlinearity is assumed
+  for this initialization scheme.
+
+  References:
+    He K, Zhang X, Ren S, Sun J. Delving deep into rectifiers: Surpassing
+    human-level performance on imagenet classification. In Proceedings of the
+    IEEE international conference on computer vision 2015 (pp. 1026-1034).
+    https://arxiv.org/abs/1502.01852
+  """
+
+  def __init__(self, seed=None, dtype=tf.float32):
+    super(TrainableHeNormal, self).__init__(
+        stddev_initializer=ScaledNormalStdDev(scale=2.0, seed=seed,
+                                              dtype=dtype),
+        seed=seed, dtype=dtype)
+
+  def get_config(self):
+    return {
+        'seed': self.seed,
+        'dtype': self.dtype.name
+    }
+
+
+class TrainableGlorotNormal(TrainableNormal):
+  """Trainable normal initialized per Glorot and Bengio, 2010.
+
+  The distribution is initialized to a Normal scaled by `sqrt(2 / fan_in +
+  fan_out)`, where `fan_in` is the number of input units and `fan_out` is the
+  number of output units.
+
+  References:
+    Glorot X, Bengio Y. Understanding the difficulty of training deep
+    feedforward neural networks. In Proceedings of the thirteenth international
+    conference on artificial intelligence and statistics 2010 Mar 31 (pp.
+    249-256). http://proceedings.mlr.press/v9/glorot10a/glorot10a.pdf
+  """
+
+  def __init__(self, seed=None, dtype=tf.float32):
+    super(TrainableGlorotNormal, self).__init__(
+        stddev_initializer=ScaledNormalStdDev(mode='fan_avg', seed=seed,
+                                              dtype=dtype),
+        seed=seed, dtype=dtype)
+
+  def get_config(self):
+    return {
+        'seed': self.seed,
+        'dtype': self.dtype.name
+    }
+
+
 def trainable_normal():  # alias, following tf.keras.initializers
   return TrainableNormal()
+
+
+def trainable_he_normal():  # alias, following tf.keras.initializers
+  return TrainableHeNormal()
+
+
+def trainable_glorot_normal():  # alias, following tf.keras.initializers
+  return TrainableGlorotNormal()
 
 
 class NormalKLDivergence(tf.keras.regularizers.Regularizer):
