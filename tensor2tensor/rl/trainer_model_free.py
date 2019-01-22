ec56d6014ddf2072a439dabaade8622093817409
blazejosinski <804945+blazejosinski@users.noreply.github.com>
2019-01-21 21:02:28 -0800

    internal merge of PR #1391
    PiperOrigin-RevId: 230279058

--- tensor2tensor/rl/trainer_model_free.py
+++ tensor2tensor/rl/trainer_model_free.py
@@ -27,8 +27,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import pprint
 import os
+import pprint
 
 from tensor2tensor.models.research import rl
 from tensor2tensor.rl import rl_utils
