74f37a45f7a0566d4a3dc79086a0598094b575db
Noam Shazeer <noam@google.com>
2019-01-28 14:13:18 -0800

    Code for Multiquery attention paper
    PiperOrigin-RevId: 231286854

--- tensor2tensor/models/__init__.py
+++ tensor2tensor/models/__init__.py
@@ -52,6 +52,7 @@ from tensor2tensor.models.research import gene_expression
 from tensor2tensor.models.research import glow
 from tensor2tensor.models.research import lm_experiments
 from tensor2tensor.models.research import moe_experiments
+from tensor2tensor.models.research import multiquery_paper
 from tensor2tensor.models.research import rl
 from tensor2tensor.models.research import similarity_transformer
 from tensor2tensor.models.research import super_lm
