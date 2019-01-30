f77b7fd0b5ae04267b8f8df49e02f9a1df255f02
David Bishai <david.bishai@asurion.com>
2019-01-30 11:27:00 -0600

    add missing import for text_cnn

--- tensor2tensor/models/__init__.py
+++ tensor2tensor/models/__init__.py
@@ -38,6 +38,7 @@ from tensor2tensor.models import resnet
 from tensor2tensor.models import revnet
 from tensor2tensor.models import shake_shake
 from tensor2tensor.models import slicenet
+from tensor2tensor.models import text_cnn
 from tensor2tensor.models import transformer
 from tensor2tensor.models import vanilla_gan
 from tensor2tensor.models import xception
