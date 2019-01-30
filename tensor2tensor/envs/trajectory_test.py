afb1917d2077ee8fc1e27fcee3610cce67a7d37e
Afroz Mohiuddin <afrozm@google.com>
2019-01-29 22:31:42 -0800

    INTERNAL
    PiperOrigin-RevId: 231535375

--- tensor2tensor/envs/trajectory_test.py
+++ tensor2tensor/envs/trajectory_test.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Tests for google3.third_party.py.tensor2tensor.envs.trajectory."""
+"""Tests for tensor2tensor.envs.trajectory."""
 
 from __future__ import absolute_import
 from __future__ import division
