8190c499acb7ecf7917e978b13cd39e82929c331
T2T Team <no-reply@google.com>
2019-02-07 16:37:01 -0800

    Evolved Transformer TPU hparams.
    PiperOrigin-RevId: 232969826

--- tensor2tensor/models/evolved_transformer.py
+++ tensor2tensor/models/evolved_transformer.py
@@ -105,3 +105,23 @@ def evolved_transformer_base():
 def evolved_transformer_big():
   """Big parameters for Evolved Transformer model on WMT."""
   return add_evolved_transformer_hparams(transformer.transformer_big())
+
+
+@registry.register_hparams
+def evolved_transformer_base_tpu():
+  """Base parameters for Evolved Transformer model on TPU."""
+  hparams = add_evolved_transformer_hparams(transformer.transformer_tpu())
+  hparams.learning_rate_constant = 1 / hparams.learning_rate_warmup_steps ** 0.5
+  hparams.learning_rate_schedule = (
+      "constant*single_cycle_cos_decay")
+  return hparams
+
+
+@registry.register_hparams
+def evolved_transformer_big_tpu():
+  """Big parameters for Evolved Transformer model on TPU."""
+  hparams = add_evolved_transformer_hparams(transformer.transformer_big_tpu())
+  hparams.learning_rate_constant = 1 / hparams.learning_rate_warmup_steps ** 0.5
+  hparams.learning_rate_schedule = (
+      "constant*single_cycle_cos_decay")
+  return hparams
