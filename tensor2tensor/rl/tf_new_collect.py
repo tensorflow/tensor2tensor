fd9f1198605c2a5acd086debbc5160200f7da3f4
piotrmilos <piotr.milos@codilime.com>
2019-02-27 13:04:06 +0100

    StackWrapper

--- tensor2tensor/rl/tf_new_collect.py
+++ tensor2tensor/rl/tf_new_collect.py
@@ -56,27 +56,50 @@ class NewSimulatedBatchEnv(NewInGraphBatchEnv):
     hidden_state_single_env = tf.zeros(self.meta_data[0][0][0][1:], self.meta_data[0][0][1]) + 10
     return hidden_state_single_env, hidden_state_single_env[...,-1:]
 
-#
-# class NewStackWrapper(NewInGraphBatchEnv):
-#
-#   def __init__(self, batch_env, history=4):
-#     super(NewStackWrapper, self).__init__(batch_env.batch_size)
-#     self.batch_env = batch_env
-#     self.history = history
-#
-#   @property
-#   def meta_data(self):
-#     hs, ob, ac, struct = self.batch_env.meta_data
-#     ob_shape, ob_type, _ = ob
-#     stack_ob_shape = [self.batch_size, self.history] + ob_shape[1:]
-#     stack_ob_spec = (stack_ob_shape, ob_type, 'observation')
-#
-#     return hs+[stack_ob_spec], stack_ob_spec, ac, struct+(1,)
-#
-  # def step(self, hidden_state, action):
+
+class NewStackWrapper(NewInGraphBatchEnv):
+  #TODO: it might be more natural to stack on the last dim
+
+  def __init__(self, batch_env, history=4):
+    super(NewStackWrapper, self).__init__(batch_env.batch_size)
+    self._env = batch_env
+    self.history = history
+
+  @property
+  def meta_data(self):
+    hs, ob, ac = self._env.meta_data
+    ob_shape, ob_type, _ = ob
+    stack_ob_shape = [self.batch_size, self.history] + ob_shape[1:]
+    stack_ob_spec = (stack_ob_shape, ob_type, 'observation')
+
+    return hs+[stack_ob_spec], stack_ob_spec, ac
+
+  def step(self, hidden_state, action):
+    env_hidden_state, stack_hidden_state = hidden_state
+    (new_env_hidden_state,), env_ob, env_reward, env_done = self._env.step((env_hidden_state,), action)
+    new_stack_hidden_state = tf.concat([stack_hidden_state[:, 1:, ...], tf.expand_dims(env_ob, axis=1)], axis=1)
+
+    return (new_env_hidden_state, new_stack_hidden_state), new_stack_hidden_state, env_reward, env_done
+
+  def reset(self, hidden_state, _, done):
+    env_hidden_state, stack_hidden_state = hidden_state
+    env_observ = stack_hidden_state[:, -1, ...]
+    (new_env_hidden_state,), new_env_observation = self._env.reset((env_hidden_state,), env_observ, done)
+
+    def extend(ob):
+      _, ob_metadata, _ = self._env.meta_data
+      multiples = (self.history,) + (1,) * (len(ob_metadata) - 1)
+      return tf.tile(tf.expand_dims(ob, axis=0), multiples)
+
+    new_stack_hidden_state, _, _ = tf.scan(lambda _, x: tf.cond(x[2],
+                                                                lambda: (extend(x[1]), x[1], tf.constant(False)),
+                                                                lambda: x),
+                                           (stack_hidden_state, new_env_observation, done),
+                                           initializer=(stack_hidden_state[0], new_env_observation[0], done[0]))
 
 
 
+    return (new_env_hidden_state, new_stack_hidden_state), new_stack_hidden_state
 
 
 def policy(ob, batch_size):
@@ -88,6 +111,7 @@ def policy(ob, batch_size):
 
 
 batch_env = NewSimulatedBatchEnv(batch_size=3)
+batch_env = NewStackWrapper(batch_env, history=4)
 
 def _new_define_collect(batch_env, hparams, force_beginning_resets):
 
@@ -140,3 +164,4 @@ def _new_define_collect(batch_env, hparams, force_beginning_resets):
 
 x = _new_define_collect(batch_env, Munch(epoch_length=10), True)
 print(x[0][:, 0, 0, 0])
+print(x[0][:, 0, :, 0, 0])
