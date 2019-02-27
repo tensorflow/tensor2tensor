7a15de567c53540a0a273d94dff6ce4e385365e2
piotrmilos <piotr.milos@codilime.com>
2019-02-27 11:54:30 +0100

    enabling compund hidden state

--- tensor2tensor/rl/tf_new_collect.py
+++ tensor2tensor/rl/tf_new_collect.py
@@ -19,16 +19,6 @@ class NewInGraphBatchEnv():
   def step(self, hidden_state, action):
     raise NotImplementedError()
 
-  def reset_one_env(self):
-    raise NotImplementedError()
-
-  def reset(self, hidden_state, observation, done):
-    hidden_state, observation, _ = tf.scan(lambda _, x: tf.cond(x[2],
-                                                                lambda: self.reset_one_env() + (tf.constant(False),),
-                                                                lambda: x),
-                                           (hidden_state, observation, done),
-                                           initializer=(hidden_state[0], observation[0], done[0]))
-    return hidden_state, observation
 
 
 
@@ -39,37 +29,52 @@ class NewSimulatedBatchEnv(NewInGraphBatchEnv):
 
   @property
   def meta_data(self):
-    return ([self.batch_size, 2, 1], tf.int32, 'hidden_state'), \
+    return [([self.batch_size, 2, 1], tf.int32, 'hidden_state')], \
            ([self.batch_size, 2, 1], tf.int32, 'observation'), \
            ([self.batch_size], tf.int32, 'action')
 
   def step(self, hidden_state, action):
-    ob = hidden_state[..., -1:] + 1
-    new_hidden_state = tf.concat([hidden_state[..., 1:], ob], axis=2)
+    hidden_state_unpacked = hidden_state[0]
+    ob = hidden_state_unpacked[..., -1:] + 1
+    new_hidden_state = tf.concat([hidden_state_unpacked[..., 1:], ob], axis=2)
     done = tf.constant([False, False, False])
     reward = tf.constant([0.1, 0.1, 0.1])
 
-    return new_hidden_state, ob, reward, done
+    return (new_hidden_state,), ob, reward, done
 
-  def reset_one_env(self):
-    hidden_state = tf.zeros(self.meta_data[0][0][1:], self.meta_data[0][1]) + 10
-    return hidden_state, hidden_state[...,-1:]
+  def reset(self, hidden_state, observation, done):
+    hidden_state_unpacked = hidden_state[0]
+    new_hidden_state, observation, _ = tf.scan(lambda _, x: tf.cond(x[2],
+                                                                lambda: self._reset_one_env() + (tf.constant(False),),
+                                                                lambda: x),
+                                           (hidden_state_unpacked, observation, done),
+                                           initializer=(hidden_state_unpacked[0], observation[0], done[0]))
+    return (new_hidden_state, ), observation
 
 
-class NewStackWrapper(NewInGraphBatchEnv):
+  def _reset_one_env(self):
+    hidden_state_single_env = tf.zeros(self.meta_data[0][0][0][1:], self.meta_data[0][0][1]) + 10
+    return hidden_state_single_env, hidden_state_single_env[...,-1:]
 
-  def __init__(self, batch_env, history=4):
-    super(NewStackWrapper, self).__init__(batch_env.batch_size)
-    self.batch_env = batch_env
-    self.history = history
+#
+# class NewStackWrapper(NewInGraphBatchEnv):
+#
+#   def __init__(self, batch_env, history=4):
+#     super(NewStackWrapper, self).__init__(batch_env.batch_size)
+#     self.batch_env = batch_env
+#     self.history = history
+#
+#   @property
+#   def meta_data(self):
+#     hs, ob, ac, struct = self.batch_env.meta_data
+#     ob_shape, ob_type, _ = ob
+#     stack_ob_shape = [self.batch_size, self.history] + ob_shape[1:]
+#     stack_ob_spec = (stack_ob_shape, ob_type, 'observation')
+#
+#     return hs+[stack_ob_spec], stack_ob_spec, ac, struct+(1,)
+#
+  # def step(self, hidden_state, action):
 
-  @property
-  def meta_data(self):
-    hs, ob, ac = self.batch_env.meta_data
-    ob_shape, ob_type, _ = ob
-    new_ob_shape = [self.batch_size, self.history] + ob_shape[1:]
-
-    return hs, (new_ob_shape, ob_type, 'observation'), ac
 
 
 
@@ -87,16 +92,15 @@ batch_env = NewSimulatedBatchEnv(batch_size=3)
 def _new_define_collect(batch_env, hparams, force_beginning_resets):
 
   batch_size = batch_env.batch_size
-  hidden_state_type, observation_type, action_type = batch_env.meta_data
+  hidden_state_types, observation_type, action_type = batch_env.meta_data
   done_type = ([batch_size], tf.bool, 'done')
 
+  ppo_data_metadata = [observation_type, ([batch_size], tf.float32, 'reward'),
+                       done_type, action_type,
+                       ([batch_size], tf.float32, 'pdf'),
+                       ([batch_size], tf.float32, 'value_function')]
 
-  ppo_data_metadata =  [observation_type, ([batch_size], tf.float32, 'reward'),
-                        done_type, action_type,
-                        ([batch_size], tf.float32, 'pdf'),
-                        ([batch_size], tf.float32, 'value_function')]
-
-  initial_state_metadata = [hidden_state_type, observation_type, done_type]
+  initial_state_metadata = hidden_state_types + [ observation_type, done_type]
 
   # These are only for typing, values will be discarded
   initial_ppo_batch = tuple(tf.zeros(shape, dtype=type) for shape, type, _ in ppo_data_metadata)
@@ -104,17 +108,21 @@ def _new_define_collect(batch_env, hparams, force_beginning_resets):
   # Below we intialize with ones, to
   # set done=True. Other fields are just for typeing.
   if force_beginning_resets:
-    initial_running_state = tuple(tf.ones(shape, dtype=type)
-                                   for shape, type, _ in initial_state_metadata)
+    initial_running_state = [tf.ones(shape, dtype=type)
+                                   for shape, type, _ in initial_state_metadata]
   else:
-    initial_running_state = tuple(
+    initial_running_state = [
       tf.get_variable(  # pylint: disable=g-complex-comprehension
         "collect_initial_running_state_%s" % (name),
         shape=shape,
         dtype=dtype,
         initializer=tf.ones_initializer(),
         trainable=False) for (shape, dtype, name) in initial_state_metadata
-    )
+    ]
+
+  initial_running_state = tf.contrib.framework.nest.pack_sequence_as(
+    ((1,) * len(hidden_state_types),) + (2, 3), initial_running_state)
+
 
   initial_batch = initial_running_state + (initial_ppo_batch,)
 
