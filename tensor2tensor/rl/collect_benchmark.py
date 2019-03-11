abd99246f1e0d480c12f5940dcc867400a02b82c
Piotr Kozakowski <kozak000@gmail.com>
2019-03-11 13:35:41 +0000

    Rewrite to TPUEstimator

--- tensor2tensor/rl/collect_benchmark.py
+++ tensor2tensor/rl/collect_benchmark.py
@@ -38,58 +38,83 @@ from time import time
 
 FLAGS = tf.flags.FLAGS
 
-tf.flags.DEFINE_bool("use_tpu", False, "Whether to use TPU.")
+tf.flags.DEFINE_string("tpu", "", "Which TPU to use.")
 
 BATCH_SIZES = [16, 32, 64, 128]
 EPOCH_LENGTHS = [50, 100, 200]
-if FLAGS.use_tpu:
+if FLAGS.tpu:
   NUMS_TPUS = [8, 4, 2, 1]
 else:
   NUMS_TPUS = [0]
 HISTORY = 4
-TPU_NAME = "ng-tpu-01"
 
 
-
-def run_config(sess, topology, num_tpus, batch_size, epoch_length):
-  if FLAGS.use_tpu:
-    batch_size //= num_tpus
-
-  with tf.variable_scope("model", reuse=tf.AUTO_REUSE):
-    batch_env = tf_new_collect.NewSimulatedBatchEnv(
-        batch_size,
-        "next_frame_basic_stochastic_discrete",
-        trainer_lib.create_hparams("next_frame_basic_stochastic_discrete_long")
+def make_model_fn(batch_size, epoch_length, num_tpus):
+  def model_fn(features, labels, mode, params):
+    with tf.variable_scope("model", reuse=tf.AUTO_REUSE):
+      batch_env = tf_new_collect.NewSimulatedBatchEnv(
+          batch_size,
+          "next_frame_basic_stochastic_discrete",
+          trainer_lib.create_hparams("next_frame_basic_stochastic_discrete_long")
+      )
+      batch_env = tf_new_collect.NewStackWrapper(batch_env, HISTORY)
+  
+    ppo_hparams = trainer_lib.create_hparams("ppo_original_params")
+    ppo_hparams.policy_network = "feed_forward_cnn_small_categorical_policy"
+    ppo_hparams.epoch_length = epoch_length
+    action_space = Discrete(2)
+    with tf.variable_scope(
+        "collect_{}_{}_{}".format(num_tpus, batch_size, epoch_length)
+    ):
+      memory = tf_new_collect.new_define_collect(
+          batch_env, ppo_hparams, action_space, force_beginning_resets=True
+      )
+    tf.get_default_graph().get_collection_ref(tf.GraphKeys.SUMMARIES)[:] = []
+    return tf.contrib.tpu.TPUEstimatorSpec(
+        mode=tf.estimator.ModeKeys.PREDICT,
+        predictions={"x": memory[1]},
     )
-    batch_env = tf_new_collect.NewStackWrapper(batch_env, HISTORY)
+  return model_fn
 
-  ppo_hparams = trainer_lib.create_hparams("ppo_original_params")
-  ppo_hparams.policy_network = "feed_forward_cnn_small_categorical_policy"
-  ppo_hparams.epoch_length = epoch_length
-  action_space = Discrete(2)
-  with tf.variable_scope(
-      "collect_{}_{}_{}".format(num_tpus, batch_size, epoch_length)
-  ):
-    memory = tf_new_collect.new_define_collect(
-        batch_env, ppo_hparams, action_space, force_beginning_resets=True
-    )
-  if FLAGS.use_tpu:
-    memory = tpu.replicate(
-        lambda: memory,
-        inputs=([()] * num_tpus),
-        device_assignment=tf.contrib.tpu.device_assignment(
-            topology, num_replicas=num_tpus
-        )
-    )
-  sess.run(tf.global_variables_initializer())
+
+def input_fn(params):
+  return tf.data.Dataset.from_tensors(tf.zeros([16]))
+
+
+def run_single(sess, run_config, num_tpus, batch_size, epoch_length):
+  #if FLAGS.tpu:
+  #  batch_size //= num_tpus
+
+  model_fn = make_model_fn(batch_size, epoch_length, num_tpus)
+  estimator = tf.contrib.tpu.TPUEstimator(
+      model_fn=model_fn,
+      use_tpu=bool(FLAGS.tpu),
+      train_batch_size=batch_size,
+      eval_batch_size=batch_size,
+      predict_batch_size=batch_size,
+      params={},
+      config=run_config,
+  )
+  return list(estimator.predict(input_fn=input_fn))
+
+  #if FLAGS.tpu:
+  #  memory = tpu.rewrite(lambda: memory)
+    #memory = tpu.replicate(
+    #    lambda: memory,
+    #    inputs=([()] * num_tpus),
+    #    device_assignment=tf.contrib.tpu.device_assignment(
+    #        topology, num_replicas=num_tpus
+    #    )
+    #)
+  #sess.run(tf.global_variables_initializer())
   # First trial is for warmup.
-  sess.run(memory)
-  t = time()
-  sess.run(memory)
-  return time() - t
+  #sess.run(memory)
+  #t = time()
+  #sess.run(memory)
+  #return time() - t
 
 
-def run_configs(run_fn, **ranges):
+def run_all(run_fn, **ranges):
   results = {}
   for (name, range) in ranges.items():
     print("looping over", name)
@@ -106,16 +131,28 @@ def run_configs(run_fn, **ranges):
 
 
 def main(_):
-  if FLAGS.use_tpu:
-    target = TPUClusterResolver(tpu=[TPU_NAME]).get_master()
+  if FLAGS.tpu:
+    resolver = TPUClusterResolver(tpu=[FLAGS.tpu])
+    target = resolver.get_master()
   else:
     target = ""
   with tf.Session(target) as sess:
-    if FLAGS.use_tpu:
+    if FLAGS.tpu:
       topology = sess.run(tpu.initialize_system())
+    else:
+      topology = None
 
-    results = run_configs(
-        functools.partial(run_config, sess, topology),
+    run_config = tf.contrib.tpu.RunConfig(
+        cluster=resolver,
+        model_dir="gs://tpu_atari_training_data/test",
+        session_config=tf.ConfigProto(
+            allow_soft_placement=True, log_device_placement=True
+        ),
+        tpu_config=tf.contrib.tpu.TPUConfig(1),  # (iterations, shards)
+    )
+
+    results = run_all(
+        functools.partial(run_single, sess, run_config),
         batch_size=BATCH_SIZES,
         epoch_length=EPOCH_LENGTHS,
         num_tpus=NUMS_TPUS,
@@ -123,7 +160,7 @@ def main(_):
     with open("results.pkl", "wb") as f:
       f.write(results)
 
-    if FLAGS.use_tpu:
+    if FLAGS.tpu:
       sess.run(tpu.shutdown_system())
 
 
