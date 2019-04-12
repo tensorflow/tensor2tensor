86283ea534497b2f97d775abd69d8c57cf59a754
T2T Team <no-reply@google.com>
2019-04-12 15:24:46 -0700

    Internal
    PiperOrigin-RevId: 243343517

--- tensor2tensor/data_generators/all_problems.py
+++ tensor2tensor/data_generators/all_problems.py
@@ -19,6 +19,7 @@ from __future__ import division
 from __future__ import print_function
 
 import importlib
+import six
 from six.moves import range  # pylint: disable=redefined-builtin
 
 MODULES = [
