e640eb09ff961f3becf7234b871a655afa332edc
Bairen Yi <byi@connect.ust.hk>
2019-04-26 02:35:19 +0800

    Update trainer_lib.py (#1556)
    Fixes #1299.
    
    Signed-off-by: Bairen Yi <byi@connect.ust.hk>

--- tensor2tensor/utils/trainer_lib.py
+++ tensor2tensor/utils/trainer_lib.py
@@ -532,7 +532,7 @@ class T2TExperiment(object):
         config.cluster_spec,
         job_name=config.task_type,
         task_index=config.task_id,
-        protocol=self._hparams.std_server_protocol)
+        protocol=config.protocol)
     server.join()
 
   def decode(self,
