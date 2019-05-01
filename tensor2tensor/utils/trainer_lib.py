869321141b6773337d8bfc51861035f5ae64dbea
Randall Lin <randall@fathomhealth.co>
2019-05-01 14:19:24 -0700

    Update trainer_lib.py

--- tensor2tensor/utils/trainer_lib.py
+++ tensor2tensor/utils/trainer_lib.py
@@ -676,6 +676,8 @@ def set_random_seed(seed):
   tf.set_random_seed(seed)
   random.seed(seed)
   np.random.seed(seed)
+  # Fathom
+  tf.logging.warn("Set random seed to: %d", FLAGS.random_seed)
 
 
 def restore_checkpoint(ckpt_dir, saver, sess, must_restore=False):
