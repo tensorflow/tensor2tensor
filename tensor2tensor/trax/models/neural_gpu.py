691abb19880664d8db5fb19cd34c0cb551465376
T2T Team <no-reply@google.com>
2019-05-07 15:56:44 -0700

    trax - fix neural_gpu imports
    PiperOrigin-RevId: 247107420

--- tensor2tensor/trax/models/neural_gpu.py
+++ tensor2tensor/trax/models/neural_gpu.py
@@ -17,7 +17,6 @@
 
 from __future__ import absolute_import
 from __future__ import division
-from __future__ import google_type_annotations
 from __future__ import print_function
 
 from tensor2tensor.trax import layers as tl
