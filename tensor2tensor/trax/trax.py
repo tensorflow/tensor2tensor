a877a71af0f0ac63a61f9640ebde52fe219acc53
Peter Hawkins <phawkins@google.com>
2019-05-22 10:32:15 -0700

    [TRAX] Move @pmap outside of predict function to avoid unnecessary recompilation during eval.
    If you write in JAX:
    ```
    def f(y):
    
      @jit
      def g(x):
        return x + 1
    
      return g(y)
    ```
    this means that *each* time you run `f` you create a new `g`. They are different Python functions, after all. JAX attaches the jit cache to the function `g`, so this means that each time you call `f` we build and `jit`-compile a new instance of `g`.
    
    The fix is easy enough, just hoist the definition of `g` outside of `f`, i.e.:
    ```
    @jit
    def g(x):
      return x + 1
    
    def f(y):
      return g(y)
    ```
    
    PiperOrigin-RevId: 249474567

--- tensor2tensor/trax/trax.py
+++ tensor2tensor/trax/trax.py
@@ -295,16 +295,18 @@ def epochs(steps=None, epoch_steps=1):
 
 def _jit_predict_fun(model_predict, num_devices):
   """Use jit on model_predict if required."""
+
+  # Multi-devices, pmap and run.
+  @functools.partial(backend.pmap, axis_name="batch")
+  def mapped_predict(x, params, rng):
+    return model_predict(x, params, rng=rng)
+
   def predict(x, params=(), rng=None):
     """Predict function jited and parallelized as requested."""
     # On one device, jit and run.
     if num_devices == 1:
       return backend.jit(model_predict)(x, params, rng=rng)
 
-    # Multi-devices, pmap and run.
-    @functools.partial(backend.pmap, axis_name="batch")
-    def mapped_predict(x, params, rng):
-      return model_predict(x, params, rng=rng)
     pred = mapped_predict(
         reshape_by_device(x, num_devices),
         params,
