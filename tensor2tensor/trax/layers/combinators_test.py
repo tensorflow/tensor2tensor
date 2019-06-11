d7f02686034e57b41f2460a08e6e489620243d26
T2T Team <no-reply@google.com>
2019-06-11 07:56:38 -0700

    Add some combinator tests.
    PiperOrigin-RevId: 252618826

--- tensor2tensor/trax/layers/combinators_test.py
+++ tensor2tensor/trax/layers/combinators_test.py
@@ -20,30 +20,94 @@ from __future__ import print_function
 
 from absl.testing import absltest
 from tensor2tensor.trax.layers import base
-from tensor2tensor.trax.layers import combinators
+from tensor2tensor.trax.layers import combinators as cb
+from tensor2tensor.trax.layers import core
+
+
+_EMPTY_STACK = ()
+_REST_OF_STACK = ((1, 5), (4,))
 
 
 class CombinatorLayerTest(absltest.TestCase):
 
+  def test_drop(self):
+    layer = cb.Drop()
+    input_shape = ((3, 2),)
+    expected_shape = _EMPTY_STACK
+    output_shape = base.check_shape_agreement(layer, input_shape)
+    self.assertEqual(output_shape, expected_shape)
+
+    input_shape = ((3, 2),) + _REST_OF_STACK
+    expected_shape = _REST_OF_STACK
+    output_shape = base.check_shape_agreement(layer, input_shape)
+    self.assertEqual(output_shape, expected_shape)
+
+  def test_dup(self):
+    layer = cb.Dup()
+    input_shape = ((3, 2),)
+    expected_shape = ((3, 2), (3, 2))
+    output_shape = base.check_shape_agreement(layer, input_shape)
+    self.assertEqual(output_shape, expected_shape)
+
+    input_shape = ((3, 2),) + _REST_OF_STACK
+    expected_shape = ((3, 2), (3, 2)) + _REST_OF_STACK
+    output_shape = base.check_shape_agreement(layer, input_shape)
+    self.assertEqual(output_shape, expected_shape)
+
+  def test_swap(self):
+    layer = cb.Swap()
+    input_shape = ((3, 2), (4, 7))
+    expected_shape = ((4, 7), (3, 2))
+    output_shape = base.check_shape_agreement(layer, input_shape)
+    self.assertEqual(output_shape, expected_shape)
+
+    input_shape = ((3, 2), (4, 7)) + _REST_OF_STACK
+    expected_shape = ((4, 7), (3, 2)) + _REST_OF_STACK
+    output_shape = base.check_shape_agreement(layer, input_shape)
+    self.assertEqual(output_shape, expected_shape)
+
+  def test_serial_no_op_list(self):
+    layer = cb.Serial([])
+    input_shape = ((3, 2), (4, 7))
+    expected_shape = ((3, 2), (4, 7))
+    output_shape = base.check_shape_agreement(layer, input_shape)
+    self.assertEqual(output_shape, expected_shape)
+
+    input_shape = ((3, 2), (4, 7)) + _REST_OF_STACK
+    expected_shape = ((3, 2), (4, 7)) + _REST_OF_STACK
+    output_shape = base.check_shape_agreement(layer, input_shape)
+    self.assertEqual(output_shape, expected_shape)
+
+  def test_serial_one_in_one_out(self):
+    layer = cb.Serial(core.Div(divisor=2.0))
+    input_shape = ((3, 2), (4, 7))
+    expected_shape = ((3, 2), (4, 7))
+    output_shape = base.check_shape_agreement(layer, input_shape)
+    self.assertEqual(output_shape, expected_shape)
+
+  def test_serial_div_div(self):
+    layer = cb.Serial(core.Div(divisor=2.0), core.Div(divisor=5.0))
+    input_shape = ((3, 2), (4, 7))
+    expected_shape = ((3, 2), (4, 7))
+    output_shape = base.check_shape_agreement(layer, input_shape)
+    self.assertEqual(output_shape, expected_shape)
+
   def test_branch(self):
     input_shape = (2, 3)
     expected_shape = ((2, 3), (2, 3))
-    output_shape = base.check_shape_agreement(
-        combinators.Branch([], []), input_shape)
+    output_shape = base.check_shape_agreement(cb.Branch([], []), input_shape)
     self.assertEqual(output_shape, expected_shape)
 
   def test_parallel(self):
     input_shape = ((2, 3), (2, 3))
     expected_shape = ((2, 3), (2, 3))
-    output_shape = base.check_shape_agreement(
-        combinators.Parallel([], []), input_shape)
+    output_shape = base.check_shape_agreement(cb.Parallel([], []), input_shape)
     self.assertEqual(output_shape, expected_shape)
 
   def test_select(self):
     input_shape = ((2, 3), (3, 4))
     expected_shape = (3, 4)
-    output_shape = base.check_shape_agreement(
-        combinators.Select(1), input_shape)
+    output_shape = base.check_shape_agreement(cb.Select(1), input_shape)
     self.assertEqual(output_shape, expected_shape)
 
 
