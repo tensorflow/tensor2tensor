8687315f65c4f3d6c67c1b609eac40a6b2e4c99f
Vinh Nguyen <vinh.nguyenx@gmail.com>
2019-07-23 13:05:41 +1000

    add _use_locking attribute to optimizer

--- tensor2tensor/utils/optimize.py
+++ tensor2tensor/utils/optimize.py
@@ -72,6 +72,7 @@ def optimize(loss, learning_rate, hparams, use_tpu=False, variables=None, gpu_au
       elif _mixed_precision_is_enabled(hparams):
           raise(RuntimeError("GPU auto mixed precision cannot be used with manual mixed precision"))
       else:
+          setattr(opt, '_use_locking', 'True')
           opt = tf.train.experimental.enable_mixed_precision_graph_rewrite(opt)
 
   opt_summaries = []
