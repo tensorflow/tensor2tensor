0c8a559bf9dc667aa9066f277053e553cfcf9d52
Afroz Mohiuddin <afrozm@google.com>
2019-08-01 13:54:47 -0700

    Give different output directories for train env and eval env. We may not want to mix up these trajectories.
    PiperOrigin-RevId: 261190975

--- tensor2tensor/trax/rlax/ppo_main.py
+++ tensor2tensor/trax/rlax/ppo_main.py
@@ -181,17 +181,26 @@ def main(argv):
   gin.parse_config_files_and_bindings(FLAGS.config_file, gin_configs)
 
   # TODO(pkozakowski): Find a better way to determine this.
+  env_kwargs = {}
+  train_env_kwargs = {}
+  eval_env_kwargs = {}
   if "OnlineTuneEnv" in FLAGS.env_problem_name:
     # TODO(pkozakowski): Separate env output dirs by train/eval and epoch.
-    env_kwargs = {"output_dir": os.path.join(FLAGS.output_dir, "envs")}
-  else:
-    env_kwargs = {}
+    train_env_kwargs = {}
+    train_env_kwargs.update(env_kwargs)
+    train_env_kwargs["output_dir"] = os.path.join(FLAGS.output_dir,
+                                                  "envs/train")
+
+    eval_env_kwargs = {}
+    eval_env_kwargs.update(env_kwargs)
+    eval_env_kwargs["output_dir"] = os.path.join(FLAGS.output_dir,
+                                                 "envs/eval")
 
   # Make an env here.
-  env = make_env(batch_size=FLAGS.batch_size, **env_kwargs)
+  env = make_env(batch_size=FLAGS.batch_size, **train_env_kwargs)
   assert env
 
-  eval_env = make_env(batch_size=FLAGS.eval_batch_size, **env_kwargs)
+  eval_env = make_env(batch_size=FLAGS.eval_batch_size, **eval_env_kwargs)
   assert eval_env
 
   def run_training_loop():
