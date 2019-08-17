669b05c20fe7515cb890e4ed9de1df37cdfc5de5
Yeming Wen <ywenxu@google.com>
2019-08-17 12:31:18 -0700

    Add TrainableNormal test in the InitializersTest.
    Implementation is based on https://github.com/tensorflow/tensorflow/blob/master/tensorflow/python/ops/init_ops_v2_test.py#L256
    
    PiperOrigin-RevId: 263958356

--- tensor2tensor/keras/initializers_test.py
+++ tensor2tensor/keras/initializers_test.py
@@ -19,6 +19,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import math
+
 import numpy as np
 from tensor2tensor.keras import initializers
 from tensor2tensor.utils import test_utils
@@ -45,6 +47,33 @@ class InitializersTest(tf.test.TestCase):
     half_cauchy_value = self.evaluate(half_cauchy)
     self.assertAllEqual(half_cauchy_value.shape, shape)
     self.assertAllGreaterEqual(half_cauchy_value, 0.)
+
+  @test_utils.run_in_graph_and_eager_modes
+  def testTrainableNormal(self):
+    shape = (100,)
+    # TrainableNormal is expected to have var 1/shape[0]
+    # because it by default has the fan_in mode scale normal std initializer.
+    initializer = initializers.get('trainable_normal')
+    normal = initializer(shape)
+    self.evaluate(tf.global_variables_initializer())
+    loc_value, scale_value = self.evaluate([
+        # Get distribution of rv -> get distribution of Independent.
+        normal.distribution.distribution.loc,
+        normal.distribution.distribution.scale])
+    fan_in = shape[0]
+    target_scale = 1.
+    target_scale /= max(1., fan_in)
+    target_scale = math.sqrt(target_scale)
+
+    self.assertAllClose(loc_value, np.zeros(shape), atol=1e-4)
+    # Tolerance is larger because of the scale normal std initializer.
+    # In this case it has std around 0.01 (0.1*target_scale).
+    self.assertAllClose(
+        scale_value, target_scale * np.ones(shape), atol=5e-2)
+
+    # Test the TrainableNormal initializer has the specified shape.
+    normal_value = self.evaluate(normal)
+    self.assertAllEqual(normal_value.shape, shape)
 
 
 if __name__ == '__main__':
