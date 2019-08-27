3a133d9be269d2d06c2291c4acb587a895b5162e
Vinh Nguyen <vinh.nguyenx@gmail.com>
2019-08-27 23:31:42 +1000

    remove unused os lib

--- tensor2tensor/utils/optimize.py
+++ tensor2tensor/utils/optimize.py
@@ -18,7 +18,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import os
 import numpy as np
 from tensor2tensor.layers import common_layers
 from tensor2tensor.utils import adafactor as adafactor_lib
