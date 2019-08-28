22820b045214feb337c44a141ca62324dfae3583
joaogui1 <joaoguilhermearujo@gmail.com>
2019-08-28 13:05:33 -0300

    Added Gaussian rectifier

--- tensor2tensor/trax/backend.py
+++ tensor2tensor/trax/backend.py
@@ -159,6 +159,7 @@ _JAX_BACKEND = {
     "np": jnp,
     "logsumexp": jax_special.logsumexp,
     "expit": jax_special.expit,
+    "erf": jax_special.erf,
     "conv": jax_conv,
     "avg_pool": jax_avg_pool,
     "max_pool": jax_max_pool,
