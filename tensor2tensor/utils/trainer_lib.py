6c84a45580ec2cc119ce188c1cf54a9b8a9ad9d3
kevincvlam <i.am.kev@gmail.com>
2019-08-28 13:17:13 -0400

    use our monitor

--- tensor2tensor/utils/trainer_lib.py
+++ tensor2tensor/utils/trainer_lib.py
@@ -352,11 +352,8 @@ def create_hooks(use_tfdbg=False,
     FLAGS = flags.FLAGS
     assert FLAGS.schedule != 'continuous_train_and_eval'
     
-    # train_hooks.append(
-    #     FathomValidationMonitor(restart_after_eval=FLAGS.restart_after_eval,
-    #         hooks=eval_hooks, **validation_monitor_kwargs))
     train_hooks.append(
-        tf.contrib.learn.monitors.ValidationMonitor(
+        FathomValidationMonitor(restart_after_eval=FLAGS.restart_after_eval,
             hooks=eval_hooks, **validation_monitor_kwargs))
 
   if use_early_stopping:
