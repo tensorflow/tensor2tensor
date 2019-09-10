d9a6d7b6327fdaf69f9f5e230977236b45a127ab
T2T Team <no-reply@google.com>
2019-09-10 14:52:06 -0700

    Add enwik8_l65k problem
    PiperOrigin-RevId: 268315466

--- tensor2tensor/data_generators/all_problems.py
+++ tensor2tensor/data_generators/all_problems.py
@@ -39,6 +39,7 @@ MODULES = [
     "tensor2tensor.data_generators.cola",
     "tensor2tensor.data_generators.common_voice",
     "tensor2tensor.data_generators.desc2code",
+    "tensor2tensor.data_generators.enwik8",
     "tensor2tensor.data_generators.fsns",
     "tensor2tensor.data_generators.function_docstring",
     "tensor2tensor.data_generators.gene_expression",
