807d47f4f6d87250fc216a9f7f70a6124f62dc82
kevincvlam <i.am.kev@gmail.com>
2019-09-20 15:06:39 -0400

    Add log statement

--- tensor2tensor/utils/trainer_lib.py
+++ tensor2tensor/utils/trainer_lib.py
@@ -628,7 +628,7 @@ def create_experiment(
   # Fathom
   # Can't add early stopping on TPU
   if not use_tpu:
-      tf.logging.debug('Turning off use_early_stopping for use_tpu')
+      tf.logging.info('Turning off use_early_stopping for use_tpu')
   use_early_stopping &= not use_tpu
 
   train_hooks, eval_hooks = create_hooks(
