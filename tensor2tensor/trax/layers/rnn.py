8b951bf0fee2dc0c816096faab296131b9d7803f
joaogui1 <joaoguilhermearujo@gmail.com>
2019-10-02 00:05:05 -0300

    First draft

--- tensor2tensor/trax/layers/rnn.py
+++ tensor2tensor/trax/layers/rnn.py
@@ -126,3 +126,110 @@ def GeneralGRUCell(candidate_transform,
       cb.Parallel(memory_transform, gate_block, candidate_block),
       cb.Gate(),
   )
+
+
+  def GRUCell(n_units):
+  """Builds a traditional GRU cell with dense internal transformations.
+
+  Gated Recurrent Unit paper: https://arxiv.org/abs/1412.3555
+
+
+  Args:
+    n_units: Number of hidden units.
+
+  Returns:
+    A Stax model representing a traditional GRU RNN cell.
+  """
+  return GeneralGRUCell(
+      candidate_transform=lambda: core.Dense(n_units),
+      memory_transform_fn=None,
+      gate_nonlinearity=core.Sigmoid,
+      candidate_nonlinearity=core.Tanh)
+
+
+def ConvGRUCell(n_units, kernel_size=(3, 3)):
+  """Builds a convolutional GRU.
+
+  Paper: https://arxiv.org/abs/1511.06432.
+
+  Args:
+    n_units: Number of hidden units
+    kernel_size: Kernel size for convolution
+
+  Returns:
+    A Stax model representing a GRU cell with convolution transforms.
+  """
+
+  def BuildConv():
+    return convolution.Conv(
+        filters=n_units, kernel_size=kernel_size, padding='SAME')
+
+  return GeneralGRUCell(
+      candidate_transform=BuildConv,
+      memory_transform_fn=None,
+      gate_nonlinearity=core.Sigmoid,
+      candidate_nonlinearity=core.Tanh)
+
+
+def GeneralLSTMCell(candidate_transform,
+                   memory_transform_fn=None,
+                   gate_nonlinearity=core.Sigmoid,
+                   candidate_nonlinearity=core.Tanh,
+                   sigmoid_bias=0.5):
+  r"""Parametrized Gated Recurrent Unit (GRU) cell construction.
+
+  GRU update equations:
+  $$ Update gate: u_t = \sigmoid(U' * s_{t-1} + B') $$
+  $$ Reset gate: r_t = \sigmoid(U'' * s_{t-1} + B'') $$
+  $$ Candidate memory: c_t = \tanh(U * (r_t \odot s_{t-1}) + B) $$
+  $$ New State: s_t = u_t \odot s_{t-1} + (1 - u_t) \odot c_t $$
+
+  See combinators.Gate for details on the gating function.
+
+
+  Args:
+    candidate_transform: Transform to apply inside the Candidate branch. Applied
+      before nonlinearities.
+    memory_transform_fn: Optional transformation on the memory before gating.
+    gate_nonlinearity: Function to use as gate activation. Allows trying
+      alternatives to Sigmoid, such as HardSigmoid.
+    candidate_nonlinearity: Nonlinearity to apply after candidate branch. Allows
+      trying alternatives to traditional Tanh, such as HardTanh
+    dropout_rate_c: Amount of dropout on the transform (c) gate. Dropout works
+      best in a GRU when applied exclusively to this branch.
+    sigmoid_bias: Constant to add before sigmoid gates. Generally want to start
+      off with a positive bias.
+
+  Returns:
+    A model representing a GRU cell with specified transforms.
+  """
+  forget_block = [  # u_t
+      cb.Dup,
+      cb.Parallel(
+      cb.Model(gate_nonlinearity(),  core.AddConstant(constant=sigmoid_bias)),
+      cb.Model(candidate_nonlinearity(),  core.AddConstant(constant=sigmoid_bias))
+      ), 
+      cb.Multiply(),
+  ]
+  input_block = [  # u_t
+      candidate_transform(),
+      core.AddConstant(constant=sigmoid_bias),
+      gate_nonlinearity(),
+  ]
+
+  output_block = [
+      cb.Model(gate_nonlinearity(),  core.AddConstant(constant=sigmoid_bias)),
+      cb.Dup(),
+      reset_block,
+      cb.Multiply(),  # Gate S{t-1} with sigmoid(candidate_transform(S{t-1}))
+      candidate_transform(),  # Final projection + tanh to get Ct
+      candidate_nonlinearity(),  # Candidate gate
+
+
+  ]
+  memory_transform = memory_transform_fn() if memory_transform_fn else []
+  return cb.Model(
+      cb.Dup(), cb.Dup(),
+      cb.Parallel(memory_transform, cb.Multiply, forget_block, candidate_block),
+      cb.Gate(),
+  )
