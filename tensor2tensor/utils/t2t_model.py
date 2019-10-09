c6189094612b3db15705ca83bdee312949537114
rllin-fathom <randall@fathomhealth.co>
2019-10-09 23:33:17 +0000

    reshape

--- tensor2tensor/utils/t2t_model.py
+++ tensor2tensor/utils/t2t_model.py
@@ -1504,7 +1504,6 @@ class T2TModel(base.Layer):
             tf.logging.warning("No key %s in logits for evaluation." % k)
         else:
           # if dataset is packed, reshape targets
-          '''
           if hasattr(problem, 'packed_length'):
             assert hasattr(problem, 'max_examples_per_pack')
             # TODO: should be able to reshape from
@@ -1515,7 +1514,6 @@ class T2TModel(base.Layer):
                 shape=[
                     problem.max_examples_per_pack,
                     hparams.max_target_seq_length, 1, 1])
-          '''
           # FATHOM
           # NOTE: right now the tf.logging.warning will not trigger.
           # ...consider if we should add
