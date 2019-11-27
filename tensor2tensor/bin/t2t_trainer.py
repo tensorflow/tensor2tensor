59e9ed5eca5e4b4b7c04b225e334abadf4e32b6b
branko-fathom <branko@fathomhealth.co>
2019-11-27 13:42:30 -0500

    Fixed checkpoint_path

--- tensor2tensor/bin/t2t_trainer.py
+++ tensor2tensor/bin/t2t_trainer.py
@@ -384,7 +384,7 @@ def main(argv):
     cloud_mlengine.launch()
     return
 
-  path = FLAGS.eval_checkpoint_path
+  path = FLAGS.eval_checkpoint_path + ".index"
   if path is not None and not fhfile.Exists(path):
       tf.logging.warn(f"Path {path} was set but unable to find")
       sys.exit(exit_codes.RESOURCE_NOT_FOUND)
