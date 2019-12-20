55248ba45431e0a079083b50eccdb0f01c469455
Cuong Hoang <cuong.hoang@agolo.com>
2019-12-20 10:56:03 +0700

    add dtypes

--- tensor2tensor/utils/multistep_optimizer.py
+++ tensor2tensor/utils/multistep_optimizer.py
@@ -28,6 +28,7 @@ from __future__ import print_function
 
 import tensorflow as tf
 from tensorflow.python.eager import context
+from tensorflow.python.framework import dtypes
 from tensorflow.python.framework import ops
 from tensorflow.python.ops import control_flow_ops
 from tensorflow.python.ops import math_ops
