a338743aa3b757c7f401ac47f6a8a8f2606d1c49
Afroz Mohiuddin <afrozm@google.com>
2020-01-10 16:45:23 -0800

    Bump up version to 1.15.4
    PiperOrigin-RevId: 289189869

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='tensor2tensor',
-    version='1.15.3',
+    version='1.15.4',
     description='Tensor2Tensor',
     long_description=(
         'Tensor2Tensor, or T2T for short, is a library of '
