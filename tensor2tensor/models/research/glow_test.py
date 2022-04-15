c8fe559e0b357389d8754474e1306b6ca9afc4f3
Yilei Yang <yileiyang@google.com>
2022-04-15 11:01:53 -0700

    Remove unused comments related to Python 2 compatibility.
    PiperOrigin-RevId: 442053350

--- tensor2tensor/models/research/glow_test.py
+++ tensor2tensor/models/research/glow_test.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Lint as: python2, python3
 """Tests for tensor2tensor.models.research.glow_model."""
 
 from __future__ import absolute_import
